7733433f65b4e4e231410235870e33332594a2c5
tonthemoon <tonthemoon@mailbox.org>
2022-05-23 14:31:51 +0000

    static musl build env

--- flake.nix
+++ flake.nix
@@ -9,26 +9,33 @@
   };
 
   outputs = { self, nixpkgs, flake-compat, flake-utils }:
-    with flake-utils.lib; eachSystem
+    let ton = { host, pkgs, static ? false }:
+      pkgs.stdenv.mkDerivation {
+        pname = "ton";
+        version = "dev";
+
+        src = ./.;
+
+        nativeBuildInputs = with host; [ cmake pkg-config git ];
+        buildInputs = with pkgs; [ openssl zlib ];
+
+        cmakeFlags = [ "-DTON_USE_ABSEIL=OFF" ] ++
+          host.lib.optionals static [ "-DCMAKE_CROSSCOMPILING=OFF"
+                                      "-DCMAKE_LINK_SEARCH_START_STATIC=ON"
+                                      "-DCMAKE_LINK_SEARCH_END_STATIC=ON" ];
+    };
+    in with flake-utils.lib; eachSystem
       (with system; [ x86_64-linux x86_64-darwin ])
       (system:
         let pkgs = nixpkgs.legacyPackages.${system};
         in {
-          defaultPackage = with pkgs; stdenv.mkDerivation {
-            pname = "ton";
-            version = "dev";
-
-            src = ./.;
-
-            nativeBuildInputs = [ cmake pkg-config git ];
-            buildInputs = [ openssl zlib ];
-
-            cmakeFlags = [ "-DTON_USE_ABSEIL=OFF" ];
-          };
-          devShell = with pkgs; mkShell {
-            nativeBuildInputs = [ cmake pkg-config git ];
-            buildInputs = [ openssl zlib ];
-          };
+          defaultPackage = ton { inherit pkgs; host = pkgs; };
         }
-    );
+      ) //
+      {
+        packages.x86_64-linux-musl.ton =
+          ton { pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgsStatic;
+                host = nixpkgs.legacyPackages.x86_64-linux;
+                static = true; };
+      };
 }
