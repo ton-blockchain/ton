9940c3a9c73af7f5808816fca2299db6322fa41d
tonthemoon <tonthemoon@mailbox.org>
2022-05-23 13:56:27 +0000

    nix package

--- flake.nix
+++ flake.nix
@@ -1,17 +1,34 @@
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-compat }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      devShell.x86_64-linux =
-        with pkgs; mkShell {
-          nativeBuildInputs = [ cmake pkg-config git ];
-          buildInputs = [ openssl zlib ];
-        };
-   };
+  outputs = { self, nixpkgs, flake-compat, flake-utils }:
+    with flake-utils.lib; eachSystem
+      (with system; [ x86_64-linux x86_64-darwin ])
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          defaultPackage = with pkgs; stdenv.mkDerivation {
+            pname = "ton";
+            version = "dev";
+
+            src = ./.;
+
+            nativeBuildInputs = [ cmake pkg-config git ];
+            buildInputs = [ openssl zlib ];
+
+            cmakeFlags = [ "-DTON_USE_ABSEIL=OFF" ];
+          };
+          devShell = with pkgs; mkShell {
+            nativeBuildInputs = [ cmake pkg-config git ];
+            buildInputs = [ openssl zlib ];
+          };
+        }
+    );
 }
