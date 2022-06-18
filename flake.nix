ac5ac72f53f616da249fd5b58dc04aadc6342b6c
tonthemoon <tonthemoon@mailbox.org>
2022-06-18 15:12:26 +0200

    nix flake: add static glibc build variant

--- flake.nix
+++ flake.nix
@@ -9,35 +9,58 @@
   };
 
   outputs = { self, nixpkgs, flake-compat, flake-utils }:
-    let ton = { host, pkgs, static ? false }:
-      pkgs.stdenv.mkDerivation {
-        pname = "ton";
-        version = "dev";
+    let
+      ton = { host, pkgs ? host, staticGlibc ? false, staticMusl ? false }:
+        with host.lib;
+        pkgs.stdenv.mkDerivation {
+          pname = "ton";
+          version = "dev";
 
-        src = ./.;
+          src = ./.;
 
-        nativeBuildInputs = with host;
-          [ cmake pkg-config git ] ++
-          [ dpkg ];
-        buildInputs = with pkgs; [ openssl zlib ];
+          nativeBuildInputs = with host;
+            [ cmake ninja pkg-config git ] ++ [ dpkg ];
+          buildInputs = with pkgs;
+            # at some point nixpkgs' pkgsStatic will build with static glibc
+            # then we can skip these manual overrides
+            # and switch between pkgsStatic and pkgsStatic.pkgsMusl for static glibc and musl builds
+            if !staticGlibc then [
+              openssl
+              zlib
+            ] else [
+              glibc.static
+              (openssl.override { static = true; })
+              (zlib.override { shared = false; })
+            ];
 
-        cmakeFlags = [ "-DTON_USE_ABSEIL=OFF" ] ++
-          host.lib.optionals static [ "-DCMAKE_CROSSCOMPILING=OFF"
-                                      "-DCMAKE_LINK_SEARCH_START_STATIC=ON"
-                                      "-DCMAKE_LINK_SEARCH_END_STATIC=ON" ];
-    };
-    in with flake-utils.lib; eachSystem
-      (with system; [ x86_64-linux x86_64-darwin ])
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          defaultPackage = ton { inherit pkgs; host = pkgs; };
-        }
-      ) //
-      {
-        packages.x86_64-linux-musl.ton =
-          ton { pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgsStatic;
-                host = nixpkgs.legacyPackages.x86_64-linux;
-                static = true; };
-      };
+          cmakeFlags = [ "-DTON_USE_ABSEIL=OFF" ]
+            ++ optionals (staticMusl || staticGlibc) [
+              "-DCMAKE_CROSSCOMPILING=OFF"
+              "-DCMAKE_LINK_SEARCH_START_STATIC=ON"
+              "-DCMAKE_LINK_SEARCH_END_STATIC=ON"
+              "-DBUILD_SHARED_LIBS=OFF"
+            ];
+          LDFLAGS =
+            optional staticGlibc "-static-libgcc -static-libstdc++ -static";
+        };
+    in with flake-utils.lib;
+    eachSystem (with system; [ x86_64-linux x86_64-darwin ]) (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        defaultPackage = ton {
+          inherit pkgs;
+          host = pkgs;
+        };
+      }) // (let host = nixpkgs.legacyPackages.x86_64-linux;
+      in {
+        packages.x86_64-linux-static.ton = ton {
+          inherit host;
+          staticGlibc = true;
+        };
+        packages.x86_64-linux-musl.ton = ton {
+          inherit host;
+          pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgsStatic;
+          staticMusl = true;
+        };
+      });
 }
