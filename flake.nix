180c97b4f9f609ef1d6160cf441e8ad38b2289c4
tonthemoon <tonthemoon@mailbox.org>
2022-08-04 13:56:29 +0200

    [skip ci] nix: static aarch64 builds

--- flake.nix
+++ flake.nix
@@ -67,59 +67,60 @@
           ];
         };
       in { defaultPackage = ton { inherit host; }; })
-    // (let host = nixpkgs-stable.legacyPackages.x86_64-linux;
-    in {
-      packages = rec {
-        #test = host.mkShell { nativeBuildInputs = [ host.cmake ]; };
-        x86_64-linux-static.ton = ton {
-          inherit host;
-          stdenv = host.makeStatic host.stdenv;
-          staticGlibc = true;
-        };
-        x86_64-linux-musl.ton = ton {
-          inherit host;
-          pkgs = nixpkgs-stable.legacyPackages.x86_64-linux.pkgsStatic;
-          staticMusl = true;
-        };
-        x86_64-linux-oldglibc.ton = (let
-          # look out for https://github.com/NixOS/nixpkgs/issues/129595 for progress on better infra for this
-          #
-          # nixos 19.09 ships with glibc 2.27
-          # we could also just override glibc source to a particular release
-          # but then we'd need to port patches as well
-          nixos1909 = (import (builtins.fetchTarball {
-            url = "https://channels.nixos.org/nixos-19.09/nixexprs.tar.xz";
-            sha256 = "1vp1h2gkkrckp8dzkqnpcc6xx5lph5d2z46sg2cwzccpr8ay58zy";
-          }) { localSystem = "x86_64-linux"; });
-          glibc227 = nixos1909.glibc // { pname = "glibc"; };
-          pkgs = import nixpkgs-stable {
-            system = "x86_64-linux";
-            overlays = [
-              # XXX
-              # https://github.com/NixOS/nixpkgs/issues/174236
-              (self: super: {
-                glibc = glibc227;
-                glibcLocales = nixos1909.glibcLocales;
-                glibcIconv = nixos1909.glibcIconv;
-                stdenv = super.stdenv // {
-                  overrides = self2: super2:
-                    super.stdenv.overrides self2 super2 // {
-                      glibc = glibc227;
-                      linuxHeaders = builtins.head glibc227.buildInputs;
-                    };
-                };
-              })
-            ];
+    // eachSystem (with system; [ x86_64-linux aarch64-linux ]) (system:
+      let host = nixpkgs-stable.legacyPackages.${system};
+      in {
+        packages = rec {
+          #test = host.mkShell { nativeBuildInputs = [ host.cmake ]; };
+          ton-static = ton {
+            inherit host;
+            stdenv = host.makeStatic host.stdenv;
+            staticGlibc = true;
           };
-        in ton {
-          inherit host;
-          inherit pkgs;
-          staticExternalDeps = true;
-        });
-        x86_64-linux-oldglibc_staticbinaries = host.symlinkJoin {
-          name = "ton";
-          paths = [ x86_64-linux-musl.ton.bin x86_64-linux-oldglibc.ton.out ];
+          ton-musl = ton {
+            inherit host;
+            pkgs = nixpkgs-stable.legacyPackages.${system}.pkgsStatic;
+            staticMusl = true;
+          };
+          ton-oldglibc = (let
+            # look out for https://github.com/NixOS/nixpkgs/issues/129595 for progress on better infra for this
+            #
+            # nixos 19.09 ships with glibc 2.27
+            # we could also just override glibc source to a particular release
+            # but then we'd need to port patches as well
+            nixos1909 = (import (builtins.fetchTarball {
+              url = "https://channels.nixos.org/nixos-19.09/nixexprs.tar.xz";
+              sha256 = "1vp1h2gkkrckp8dzkqnpcc6xx5lph5d2z46sg2cwzccpr8ay58zy";
+            }) { localSystem = "x86_64-linux"; });
+            glibc227 = nixos1909.glibc // { pname = "glibc"; };
+            pkgs = import nixpkgs-stable {
+              inherit system;
+              overlays = [
+                # XXX
+                # https://github.com/NixOS/nixpkgs/issues/174236
+                (self: super: {
+                  glibc = glibc227;
+                  glibcLocales = nixos1909.glibcLocales;
+                  glibcIconv = nixos1909.glibcIconv;
+                  stdenv = super.stdenv // {
+                    overrides = self2: super2:
+                      super.stdenv.overrides self2 super2 // {
+                        glibc = glibc227;
+                        linuxHeaders = builtins.head glibc227.buildInputs;
+                      };
+                  };
+                })
+              ];
+            };
+          in ton {
+            inherit host;
+            inherit pkgs;
+            staticExternalDeps = true;
+          });
+          ton-oldglibc_staticbinaries = host.symlinkJoin {
+            name = "ton";
+            paths = [ ton-musl.bin ton-oldglibc.out ];
+          };
         };
-      };
-    });
+      });
 }
