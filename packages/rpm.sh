fd4c0be37c509d83c5caabc8b9e19a8ec5650198
tonthemoon <tonthemoon@mailbox.org>
2022-08-05 16:13:48 +0200

    packages: deb,rpm multiarch

--- packages/rpm.sh
+++ packages/rpm.sh
@@ -8,6 +8,7 @@ BUILD_PATH="$1"
 RPM_TEMPLATE_PATH="$2"
 RPMBUILD_PATH=$(dirname $(dirname "$2"))
 NIX_RESULT_PATH="$3"
+PACKAGE_ARCH="$4"
 RPM_INSTALL_PATH="$BUILD_PATH"/rpm-install
 
 mkdir -p "$RPMBUILD_PATH"/{BUILD,BUILDROOT,RPMS,SOURCES,SPECS,SRPMS}
@@ -19,7 +20,8 @@ rpmbuild --define "_topdir $RPMBUILD_PATH" \
          --define "_exec_prefix /usr" \
          --define "_bindir /usr/bin" \
          --define "releasever $(date +%s)" \
+         --target "$PACKAGE_ARCH" \
          -v -bb "$RPM_TEMPLATE_PATH"
 
 mkdir -p "$RPM_INSTALL_PATH"
-cp -r "$RPMBUILD_PATH"/RPMS/* "$RPM_INSTALL_PATH"
+cp -r "$RPMBUILD_PATH"/RPMS/"$PACKAGE_ARCH" "$RPM_INSTALL_PATH"
--- packages/rpm/SPECS/ton.spec
+++ packages/rpm/SPECS/ton.spec
@@ -6,7 +6,7 @@ Summary:        The Open Network
 License:        LGPLv2
 Source0:        ton.tar.gz
 
-BuildArch:      x86_64
+%global __os_install_post %{_usr}/lib/rpm/brp-compress %{nil} # disable brp-strip
 
 %description
 A collection of The Open Network core software and utilities.
@@ -18,7 +18,6 @@ A collection of The Open Network core software and utilities.
 %define have_lib %( if [ -f lib ]; then echo "1" ; else echo "0"; fi )
 
 %install
-
 mkdir -p %{buildroot}/%{_bindir} %{buildroot}/%{_libdir}
 
 cp -a bin/* %{buildroot}/%{_bindir}
