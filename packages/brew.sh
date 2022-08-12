28af579c3db06a5f2c8190edccf2867989d995a4
tonthemoon <tonthemoon@mailbox.org>
2022-08-12 12:34:10 +0200

    [skip ci] brew: move formula over to homebrew-ton-repo

--- packages/brew.sh
+++ packages/brew.sh
@@ -13,5 +13,4 @@ BREW_INSTALL_PATH="$BUILD_PATH"/brew-install
 SRC_INSTALL_PATH="$BREW_INSTALL_PATH"/ton-"$ARCH".tar.gz
 
 mkdir -p "$BREW_INSTALL_PATH"
-tar --create --file "$SRC_INSTALL_PATH" --transform 's,^,ton/,' -C "$NIX_RESULT_PATH" .
-cp "$FORMULA_TEMPLATE_PATH"/ton.rb "$BREW_INSTALL_PATH"/ton.rb
+tar --create --file "$SRC_INSTALL_PATH" -C "$NIX_RESULT_PATH" .
