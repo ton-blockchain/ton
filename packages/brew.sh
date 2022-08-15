e3ff728d7b181bf997691e7973023a9483fc81e0
tonthemoon <tonthemoon@mailbox.org>
2022-08-15 14:16:08 +0200

    [skip ci] fix brew install
    fixes
    Errno::EACCES: Permission denied @ dir_s_mkdir - /private/tmp/ton-XXX/bin/.brew_home

--- packages/brew.sh
+++ packages/brew.sh
@@ -13,4 +13,4 @@ BREW_INSTALL_PATH="$BUILD_PATH"/brew-install
 SRC_INSTALL_PATH="$BREW_INSTALL_PATH"/ton-"$ARCH".tar.gz
 
 mkdir -p "$BREW_INSTALL_PATH"
-tar --create --file "$SRC_INSTALL_PATH" -C "$NIX_RESULT_PATH" .
+tar --create --mode 755 -h --file "$SRC_INSTALL_PATH" -C "$NIX_RESULT_PATH" .
