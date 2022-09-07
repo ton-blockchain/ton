ceebf6cfe041116e0c9d9dac6eed25eab7a43965
AlexeyFSL <a.kokinos@fslabs.io>
2022-09-07 16:56:05 +0700

    fix funcfift lib fix CMakeFile

--- crypto/funcfiftlib/funcfiftlib.cpp
+++ crypto/funcfiftlib/funcfiftlib.cpp
@@ -26,105 +26,94 @@
     Copyright 2017-2020 Telegram Systems LLP
 */
 #include "func/func.h"
-#include "parser/srcread.h"
-#include "parser/lexer.h"
-#include "parser/symtable.h"
-#include <getopt.h>
-#include <fstream>
 #include "git.h"
 #include "td/utils/JsonBuilder.h"
+#include "fift/utils.h"
+#include "td/utils/base64.h"
+#include <sstream>
+#include <iomanip>
 
+std::string escape_json(const std::string &s) {
+  std::ostringstream o;
+  for (auto c = s.cbegin(); c != s.cend(); c++) {
+    switch (*c) {
+      case '"': o << "\\\""; break;
+      case '\\': o << "\\\\"; break;
+      case '\b': o << "\\b"; break;
+      case '\f': o << "\\f"; break;
+      case '\n': o << "\\n"; break;
+      case '\r': o << "\\r"; break;
+      case '\t': o << "\\t"; break;
+      default:
+        if ('\x00' <= *c && *c <= '\x1f') {
+          o << "\\u"
+            << std::hex << std::setw(4) << std::setfill('0') << static_cast<int>(*c);
+        } else {
+          o << *c;
+        }
+    }
+  }
+  return o.str();
+}
+
+td::Result<std::string> compile_internal(char *config_json) {
+  TRY_RESULT(input_json, td::json_decode(td::MutableSlice(config_json)))
+  auto &obj = input_json.get_object();
+
+  TRY_RESULT(opt_level, td::get_json_object_int_field(obj, "optLevel", false));
+  TRY_RESULT(sources_obj, td::get_json_object_field(obj, "sources", td::JsonValue::Type::Array, false));
+
+  auto &sources_arr = sources_obj.get_array();
+
+  std::vector<std::string> sources;
+
+  for (auto &item : sources_arr) {
+    sources.push_back(item.get_string().str());
+  }
+
+  funC::opt_level = std::max(0, opt_level);
+  funC::program_envelope = true;
+  funC::verbosity = 0;
+  funC::indent = 1;
+
+  std::ostringstream outs, errs;
+  auto compile_res = funC::func_proceed(sources, outs, errs);
+
+  if (compile_res != 0) {
+    return td::Status::Error(std::string("Func compilation error: ") + errs.str());
+  }
+
+  TRY_RESULT(code_cell, fift::compile_asm(outs.str(), "/fiftlib/", false));
+  TRY_RESULT(boc, vm::std_boc_serialize(code_cell));
+
+  td::JsonBuilder result_json;
+  auto result_obj = result_json.enter_object();
+  result_obj("status", "ok");
+  result_obj("code_boc", td::base64_encode(boc));
+  result_obj("fift_code", escape_json(outs.str()));
+  result_obj("func_errors", errs.str());
+  result_obj.leave();
+
+  outs.clear();
+  errs.clear();
+
+  return result_json.string_builder().as_cslice().str();
+}
 
 extern "C" {
 
-const char* func_compile(char* config_json) {
+const char *func_compile(char *config_json) {
+  auto res = compile_internal(config_json);
 
-  std::ostringstream outs, errs;
-  char *json_ret;
-
-  printf("config_json:\n %s\n", config_json);
-
-  try {
-
-    std::string json_str = std::string(config_json);
-
-    auto res_conf = td::json_decode(json_str);
-    if (res_conf.is_error()) {
-      errs << res_conf.error().message().c_str();
-      throw std::string("JSON decode error");
-    }
-    td::JsonValue conf_val = res_conf.move_as_ok();
-    td::JsonObject &config_obj = conf_val.get_object();
-
-    auto res_srcs = td::get_json_object_field(config_obj, "sources", td::JsonValue::Type::Array, false);
-    if (res_srcs.is_error()) {
-      errs << res_srcs.error().message().c_str();
-      throw std::string("JSON decode error");
-    }
-
-    td::JsonValue srcs_val = res_srcs.move_as_ok();
-    td::JsonArray &sources_arr = srcs_val.get_array();
-
-    std::vector<std::string> sources;
-    for (auto &src_obj : sources_arr) {
-      auto src = src_obj.get_string().str();
-      sources.push_back(src);
-    }
-
-    printf("sources:\n");
-    for (auto &src : sources) {
-      printf("%s\n", src.c_str());
-    }
-
-    auto res_opt_lvl = td::get_json_object_int_field(config_obj, "optLevel", false);
-    if (res_opt_lvl.is_error()) {
-      errs << res_opt_lvl.error().message().c_str();
-      throw std::string("JSON decode error");
-    }
-
-    funC::opt_level = std::max(0, res_opt_lvl.move_as_ok());
-
-    printf("opt_level: %d\n", funC::opt_level);
-
-    funC::program_envelope = true;
-    funC::verbosity = 0;
-    funC::indent = 1;
-
-    td::JsonBuilder result_json;
-    auto result_obj = result_json.enter_object();
-
-    if (funC::func_proceed(sources, outs, errs)) {
-      throw std::string("func compiling");
-    }
-
-    auto func_res = td::JsonBuilder();
-    auto func_o = func_res.enter_object();
-
-    func_o("fiftCode", outs.str());
-    func_o.leave();
-
-    result_obj("funcResult", func_res.string_builder().as_cslice().c_str());
-    outs.clear();
-    errs.clear();
-
-    result_obj.leave();
-    json_ret = strdup(result_json.string_builder().as_cslice().c_str());
-  }
-  catch(std::string reason) {
-      auto error_res = td::JsonBuilder();
-      auto error_o = error_res.enter_object();
-      error_o("status", "error");
-      error_o("reason", reason);
-      error_o("message", errs.str());
-      error_o.leave();
-      json_ret = strdup(error_res.string_builder().as_cslice().c_str());
-  }
-  catch(...) {
-     printf("undefined exeption type\n");
+  if (res.is_error()) {
+    auto error_res = td::JsonBuilder();
+    auto error_o = error_res.enter_object();
+    error_o("status", "error");
+    error_o("message", res.move_as_error().message().str());
+    error_o.leave();
+    return strdup(error_res.string_builder().as_cslice().c_str());
   }
 
-  return json_ret;
-
+  return res.move_as_ok().c_str();
 }
-
 }
