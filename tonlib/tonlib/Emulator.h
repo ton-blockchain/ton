79d042396cf5190c1d8095f5cbd5f669757b7e0c
legaii <jgates.ardux@gmail.com>
2022-09-09 13:26:33 +0300

    Refactor & fixes

--- tonlib/tonlib/Emulator.h
+++ tonlib/tonlib/Emulator.h
@@ -6,9 +6,11 @@
 #include "block/mc-config.h"
 
 namespace emulator {
-td::Status emulate_transactions(vm::Dictionary&& libraries, block::Config&& config, block::StdAddress address, ton::UnixTime now,
+// mc_state_root : ^ShardStateUnsplit
+// transactions : std::vector<^Transaction>
+td::Status emulate_transactions(td::Ref<vm::Cell>&& mc_state_root, block::StdAddress address, ton::UnixTime now,
                                 td::Ref<vm::CellSlice>&& shard_account_cell_slice, ton::Bits256&& rand_seed,
-                                std::vector<block::gen::Transaction::Record>&& transactions,
+                                std::vector<td::Ref<vm::Cell>>&& transactions,
                                 td::int64& balance, ton::UnixTime& storage_last_paid, vm::CellStorageStat& storage_stat,
                                 td::Ref<vm::Cell>& code, td::Ref<vm::Cell>& data, td::Ref<vm::Cell>& state,
                                 std::string& frozen_hash, ton::LogicalTime& last_trans_lt, ton::Bits256& last_trans_hash, td::uint32& gen_utime);
