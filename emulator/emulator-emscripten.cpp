afbed093c65c78052b503dfe687d974145bc622d
krigga <krigga7@gmail.com>
2022-11-27 20:13:07 +0300

    Added run_get_method to transaction emulator emscipten wrapper

--- emulator/emulator-emscripten.cpp
+++ emulator/emulator-emscripten.cpp
@@ -40,6 +40,64 @@ td::Result<TransactionEmulationParams> decode_transaction_emulation_params(const
   return params;
 }
 
+struct GetMethodParams {
+  std::string code;
+  std::string data;
+  int verbosity;
+  td::optional<std::string> libs;
+  std::string address;
+  uint32_t unixtime;
+  uint64_t balance;
+  std::string rand_seed_hex;
+  int64_t gas_limit;
+  int method_id;
+};
+
+td::Result<GetMethodParams> decode_get_method_params(const char* json) {
+  GetMethodParams params;
+
+  std::string json_str(json);
+  TRY_RESULT(input_json, td::json_decode(td::MutableSlice(json_str)));
+  auto &obj = input_json.get_object();
+
+  TRY_RESULT(code, td::get_json_object_string_field(obj, "code", false));
+  params.code = code;
+
+  TRY_RESULT(data, td::get_json_object_string_field(obj, "data", false));
+  params.data = data;
+
+  TRY_RESULT(verbosity, td::get_json_object_int_field(obj, "verbosity", false));
+  params.verbosity = verbosity;
+
+  TRY_RESULT(libs, td::get_json_object_string_field(obj, "libs", true));
+  if (libs.size() > 0) {
+    params.libs = libs;
+  }
+
+  TRY_RESULT(address, td::get_json_object_string_field(obj, "address", false));
+  params.address = address;
+
+  TRY_RESULT(unixtime_field, td::get_json_object_field(obj, "unixtime", td::JsonValue::Type::Number, false));
+  TRY_RESULT(unixtime, td::to_integer_safe<td::uint32>(unixtime_field.get_number()));
+  params.unixtime = unixtime;
+
+  TRY_RESULT(balance_field, td::get_json_object_field(obj, "balance", td::JsonValue::Type::String, false));
+  TRY_RESULT(balance, td::to_integer_safe<td::uint64>(balance_field.get_string()));
+  params.balance = balance;
+
+  TRY_RESULT(rand_seed_str, td::get_json_object_string_field(obj, "rand_seed", false));
+  params.rand_seed_hex = rand_seed_str;
+
+  TRY_RESULT(gas_limit_field, td::get_json_object_field(obj, "gas_limit", td::JsonValue::Type::String, false));
+  TRY_RESULT(gas_limit, td::to_integer_safe<td::uint64>(gas_limit_field.get_string()));
+  params.gas_limit = gas_limit;
+
+  TRY_RESULT(method_id, td::get_json_object_int_field(obj, "method_id", false));
+  params.method_id = method_id;
+
+  return params;
+}
+
 extern "C" {
 
 const char *emulate(const char *config, const char* libs, int verbosity, const char* account, const char* message, const char* params) {
@@ -48,12 +106,9 @@ const char *emulate(const char *config, const char* libs, int verbosity, const c
     td::log_interface = &logger;
     SET_VERBOSITY_LEVEL(verbosity_DEBUG);
 
-    const char* output = nullptr;
-
     auto decoded_params_res = decode_transaction_emulation_params(params);
     if (decoded_params_res.is_error()) {
-        output = strdup(R"({"fail":true,"message":"Can't decode other params"})");
-        return output;
+        return strdup(R"({"fail":true,"message":"Can't decode other params"})");
     }
     auto decoded_params = decoded_params_res.move_as_ok();
 
@@ -70,14 +125,14 @@ const char *emulate(const char *config, const char* libs, int verbosity, const c
         !transaction_emulator_set_ignore_chksig(em, decoded_params.ignore_chksig) ||
         !rand_seed_set) {
         transaction_emulator_destroy(em);
-        output = strdup(R"({"fail":true,"message":"Can't set params"})");
-        return output;
+        return strdup(R"({"fail":true,"message":"Can't set params"})");
     }
 
     auto tx = transaction_emulator_emulate_transaction(em, account, message);
 
     transaction_emulator_destroy(em);
 
+    const char* output = nullptr;
     {
         td::JsonBuilder jb;
         auto json_obj = jb.enter_object();
@@ -87,6 +142,46 @@ const char *emulate(const char *config, const char* libs, int verbosity, const c
         output = strdup(jb.string_builder().as_cslice().c_str());
     }
     free((void*) tx);
+
+    return output;
+}
+
+const char *run_get_method(const char *params, const char* stack, const char* config) {
+    StringLog logger;
+
+    td::log_interface = &logger;
+    SET_VERBOSITY_LEVEL(verbosity_DEBUG);
+
+    auto decoded_params_res = decode_get_method_params(params);
+    if (decoded_params_res.is_error()) {
+        return strdup(R"({"fail":true,"message":"Can't decode params"})");
+    }
+    auto decoded_params = decoded_params_res.move_as_ok();
+
+    auto tvm = tvm_emulator_create(decoded_params.code.c_str(), decoded_params.data.c_str(), decoded_params.verbosity);
+
+    if ((decoded_params.libs && !tvm_emulator_set_libraries(tvm, decoded_params.libs.value().c_str())) ||
+        !tvm_emulator_set_c7(tvm, decoded_params.address.c_str(), decoded_params.unixtime,
+          decoded_params.balance, decoded_params.rand_seed_hex.c_str(), config) ||
+        (decoded_params.gas_limit > 0 && !tvm_emulator_set_gas_limit(tvm, decoded_params.gas_limit))) {
+      tvm_emulator_destroy(tvm);
+      return strdup(R"({"fail":true,"message":"Can't set params"})");
+    }
+
+    auto res = tvm_emulator_run_get_method(tvm, decoded_params.method_id, stack);
+
+    tvm_emulator_destroy(tvm);
+
+    const char* output = nullptr;
+    {
+        td::JsonBuilder jb;
+        auto json_obj = jb.enter_object();
+        json_obj("output", td::JsonRaw(td::Slice(res)));
+        json_obj("logs", logger.get_string());
+        json_obj.leave();
+        output = strdup(jb.string_builder().as_cslice().c_str());
+    }
+    free((void*) res);
 
     return output;
 }
