9105cb30431995b56f6ad176a6e6f0b11cc22bdb
SpyCheese <mikle98@yandex.ru>
2022-12-28 17:31:31 +0300

    Hide excessive logs

--- rldp2/BdwStats.cpp
+++ rldp2/BdwStats.cpp
@@ -40,7 +40,7 @@ void BdwStats::on_packet_ack(const PacketInfo &info, td::Timestamp sent_at, td::
   auto ack_passed = now.at() - info.delivered_now.at();
   auto passed = td::max(sent_passed, ack_passed);
   if (passed < 0.01) {
-    VLOG(RLDP_WARNING) << "Invalid passed " << passed;
+    VLOG(RLDP_INFO) << "Invalid passed " << passed;
   }
   auto delivered = delivered_count - info.delivered_count;
   on_rate_sample((double)delivered / passed, now, info.is_paused);
