8b46235767b428caace97ef45835208f041eafec
neodiX <neodix@ton.org>
2023-07-10 13:11:18 +0200

    nix might reuse embedded libs, but for now let's use found libs

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
           src = ./.;
 
           nativeBuildInputs = with host;
-            [ cmake ninja pkg-config git ] ++
+            [ cmake ninja pkg-config git autoconf automake libtool texinfo ] ++
             optionals stdenv.isLinux [ dpkg rpm createrepo_c pacman ];
           buildInputs = with pkgs;
           # at some point nixpkgs' pkgsStatic will build with static glibc
