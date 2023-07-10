903a123410b822c6fd03bc0f42df38d2261389a6
neodiX <neodix@ton.org>
2023-07-10 20:03:44 +0200

    fix linking dependencies

--- CMake/BuildSECP256K1.cmake
+++ CMake/BuildSECP256K1.cmake
@@ -3,24 +3,32 @@ set(SECP256K1_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/third-party/secp256k1)
 set(SECP256K1_INCLUDE_DIR ${SECP256K1_BINARY_DIR}/include)
 
 if (NOT SECP256K1_LIB)
-    if (WIN32)
-      set(SECP256K1_LIB ${SECP256K1_BINARY_DIR}/lib/libsecp256k1.lib)
-    else()
-      set(SECP256K1_LIB ${SECP256K1_BINARY_DIR}/lib/libsecp256k1.so)
-    endif()
 
     file(MAKE_DIRECTORY ${SECP256K1_BINARY_DIR})
 
-    add_custom_command(
-      WORKING_DIRECTORY ${SECP256K1_SOURCE_DIR}
-      COMMAND ./autogen.sh
-      COMMAND ./configure --enable-module-recovery --prefix ${SECP256K1_BINARY_DIR}
-      COMMAND make
-      COMMAND make install
-      COMMENT "Build secp256k1"
-      DEPENDS ${SECP256K1_SOURCE_DIR}
-      OUTPUT ${SECP256K1_LIB}
-    )
+    if (WIN32)
+      set(SECP256K1_LIB ${SECP256K1_SOURCE_DIR}/bin/x64/Release/v142/static/secp256k1.lib)
+      add_custom_command(
+        WORKING_DIRECTORY ${SECP256K1_SOURCE_DIR}
+        COMMAND cd builds\msvc\vs2017
+        COMMAND msbuild /p:Configuration=StaticRelease -p:PlatformToolset=v142 -p:Platform=x64
+        COMMENT "Build secp256k1"
+        DEPENDS ${SECP256K1_SOURCE_DIR}
+        OUTPUT ${SECP256K1_LIB}
+      )
+    else()
+      set(SECP256K1_LIB ${SECP256K1_BINARY_DIR}/lib/libsecp256k1.so)
+      add_custom_command(
+          WORKING_DIRECTORY ${SECP256K1_SOURCE_DIR}
+          COMMAND ./autogen.sh
+          COMMAND ./configure --disable-option-checking --enable-module-recovery --prefix ${SECP256K1_BINARY_DIR} --with-pic --disable-shared --enable-static --disable-shared --disable-tests --disable-benchmark
+          COMMAND make
+          COMMAND make install
+          COMMENT "Build secp256k1"
+          DEPENDS ${SECP256K1_SOURCE_DIR}
+          OUTPUT ${SECP256K1_LIB}
+      )
+    endif()
 else()
    message(STATUS "Use secp256k1: ${SECP256K1_LIB}")
 endif()
--- CMake/BuildSODIUM.cmake
+++ CMake/BuildSODIUM.cmake
@@ -3,25 +3,31 @@ set(SODIUM_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/third-party/sodium)
 set(SODIUM_INCLUDE_DIR ${SODIUM_BINARY_DIR}/include)
 
 if (NOT SODIUM_LIBRARY_RELEASE)
+    file(MAKE_DIRECTORY ${SODIUM_BINARY_DIR})
     if (WIN32)
       set(SODIUM_LIBRARY_RELEASE ${SODIUM_BINARY_DIR}/lib/libsodium.lib)
+      add_custom_command(
+        WORKING_DIRECTORY ${SODIUM_SOURCE_DIR}
+        COMMAND set LIBSODIUM_FULL_BUILD=1
+        COMMAND msbuild /p:Configuration=StaticRelease -p:PlatformToolset=v142 -p:Platform=x64
+        COMMENT "Build sodium"
+        DEPENDS ${SODIUM_SOURCE_DIR}
+        OUTPUT ${SODIUM_LIBRARY_RELEASE}
+      )
     else()
       set(SODIUM_LIBRARY_RELEASE ${SODIUM_BINARY_DIR}/lib/libsodium.so)
+      add_custom_command(
+        WORKING_DIRECTORY ${SODIUM_SOURCE_DIR}
+        COMMAND export LIBSODIUM_FULL_BUILD=1
+        COMMAND ./autogen.sh
+        COMMAND ./configure --prefix ${SODIUM_BINARY_DIR}
+        COMMAND make
+        COMMAND make install
+        COMMENT "Build sodium"
+        DEPENDS ${SODIUM_SOURCE_DIR}
+        OUTPUT ${SODIUM_LIBRARY_RELEASE}
+      )
     endif()
-
-    file(MAKE_DIRECTORY ${SODIUM_BINARY_DIR})
-
-    add_custom_command(
-      WORKING_DIRECTORY ${SODIUM_SOURCE_DIR}
-      COMMAND export LIBSODIUM_FULL_BUILD=1
-      COMMAND ./autogen.sh
-      COMMAND ./configure --prefix ${SODIUM_BINARY_DIR}
-      COMMAND make
-      COMMAND make install
-      COMMENT "Build sodium"
-      DEPENDS ${SODIUM_SOURCE_DIR}
-      OUTPUT ${SODIUM_LIBRARY_RELEASE}
-    )
 else()
    message(STATUS "Use sodium: ${SODIUM_LIBRARY_RELEASE}")
 endif()
