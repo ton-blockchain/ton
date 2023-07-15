b69b33982d10d6ce295a2829f82615011a1435a2
neodiX <neodix@ton.org>
2023-07-15 20:48:00 +0200

    multipe build fixes

--- .github/script/fift-func-wasm-build-ubuntu.sh
+++ .github/script/fift-func-wasm-build-ubuntu.sh
@@ -13,14 +13,6 @@ cd third-party/zlib; make clean; cd ../..
 rm -rf openssl emsdk build
 echo `pwd`
 
-git clone https://github.com/openssl/openssl.git
-cd openssl
-git checkout OpenSSL_1_1_1j
-./config
-make -j16
-OPENSSL_DIR=`pwd`
-cd ..
-
 mkdir build
 cd build
 cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DTON_USE_ABSEIL=OFF ..
@@ -51,20 +43,9 @@ export CC=$(which emcc)
 export CXX=$(which em++)
 export CCACHE_DISABLE=1
 
-cd ../openssl
-
-make clean
-emconfigure ./Configure linux-generic32 no-shared no-dso no-engine no-unit-test no-ui no-tests
-sed -i 's/CROSS_COMPILE=.*/CROSS_COMPILE=/g' Makefile
-sed -i 's/-ldl//g' Makefile
-sed -i 's/-O3/-Os/g' Makefile
-emmake make depend
-emmake make -j16
-test $? -eq 0 || { echo "Can't compile OpenSSL with emmake "; exit 1; }
-
 cd ../build
 
-emcmake cmake -DUSE_EMSCRIPTEN=ON -DCMAKE_BUILD_TYPE=Release -DOPENSSL_ROOT_DIR=$OPENSSL_DIR -DOPENSSL_INCLUDE_DIR=$OPENSSL_DIR/include -DOPENSSL_CRYPTO_LIBRARY=$OPENSSL_DIR/libcrypto.a -DOPENSSL_SSL_LIBRARY=$OPENSSL_DIR/libssl.a -DCMAKE_TOOLCHAIN_FILE=$EMSDK_DIR/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake ..
+emcmake cmake -DUSE_EMSCRIPTEN=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$EMSDK_DIR/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake ..
 test $? -eq 0 || { echo "Can't configure TON with with emmake "; exit 1; }
 cp -R ../crypto/smartcont ../crypto/fift/lib crypto
 
