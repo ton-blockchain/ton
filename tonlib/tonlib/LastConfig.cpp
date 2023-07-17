e64d160a4f6052df0cf49ad557785a6a402d0cdc
SpyCheese <mikle98@yandex.ru>
2023-07-17 17:11:18 +0300

    Fix extracting version from config in tonlib

--- tonlib/tonlib/LastConfig.cpp
+++ tonlib/tonlib/LastConfig.cpp
@@ -93,7 +93,8 @@ td::Status LastConfig::process_config_proof(ton::ton_api::object_ptr<ton::lite_a
   }
   TRY_RESULT(state, block::check_extract_state_proof(blkid, raw_config->state_proof_.as_slice(),
                                                      raw_config->config_proof_.as_slice()));
-  TRY_RESULT(config, block::ConfigInfo::extract_config(std::move(state), block::ConfigInfo::needPrevBlocks));
+  TRY_RESULT(config, block::ConfigInfo::extract_config(
+                         std::move(state), block::ConfigInfo::needPrevBlocks | block::ConfigInfo::needCapabilities));
 
   for (auto i : params_) {
     VLOG(last_config) << "ConfigParam(" << i << ") = ";
