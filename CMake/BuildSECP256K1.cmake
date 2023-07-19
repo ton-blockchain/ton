edc4723ca3076b2912574c8bd9dffeea7c9592b7
neodiX <neodix@ton.org>
2023-07-19 15:25:28 +0200

    compile sec256k1 without msbuild

--- CMake/BuildSECP256K1.cmake
+++ CMake/BuildSECP256K1.cmake
@@ -10,12 +10,12 @@ if (NOT SECP256K1_LIBRARY)
 
     if (MSVC)
       set(SECP256K1_BINARY_DIR ${CMAKE_CURRENT_SOURCE_DIR}/third-party/secp256k1)
-      set(SECP256K1_LIBRARY ${SECP256K1_SOURCE_DIR}/bin/x64/Release/v142/static/secp256k1.lib)
+      set(SECP256K1_LIBRARY ${SECP256K1_SOURCE_DIR}/build/src/Release/libsecp256k1.lib)
       set(SECP256K1_INCLUDE_DIR ${SECP256K1_BINARY_DIR}/include)
       add_custom_command(
         WORKING_DIRECTORY ${SECP256K1_SOURCE_DIR}
-        COMMAND cd builds/msvc/vs2017
-        COMMAND msbuild /m /v:n /p:Configuration=StaticRelease -p:PlatformToolset=v142 -p:Platform=x64
+        COMMAND cmake -E env CFLAGS="/WX" cmake -A x64 -B build -DSECP256K1_ENABLE_MODULE_RECOVERY=ON -DSECP256K1_BUILD_EXAMPLES=ON -DBUILD_SHARED_LIBS=OFF
+        COMMAND cmake --build build --config Release
         COMMENT "Build secp256k1 with vs2017"
         DEPENDS ${SECP256K1_SOURCE_DIR}
         OUTPUT ${SECP256K1_LIBRARY}
