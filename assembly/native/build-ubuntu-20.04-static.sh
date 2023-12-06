f98117c23a6a2eba85427480038dc8b3d9ab9b27
neodiX <neodix@ton.org>
2023-12-07 00:27:45 +0100

    weird, fixing - cp: missing destination file operand after 'assembly/native/build-ubuntu-20.04-shared.sh'

--- assembly/native/build-ubuntu-20.04-static.sh
+++ assembly/native/build-ubuntu-20.04-static.sh
@@ -128,9 +128,19 @@ cmake -GNinja .. \
 
 test $? -eq 0 || { echo "Can't configure ton"; exit 1; }
 
+if [ $1 == "with-tests"]; then
+ninja storage-daemon storage-daemon-cli fift func tonlib tonlibjson tonlib-cli \
+      validator-engine lite-client pow-miner validator-engine-console \
+      generate-random-id json2tlo dht-server http-proxy rldp-http-proxy \
+      adnl-proxy create-state emulator test-ed25519 test-ed25519-crypto test-bigint \
+      test-vm test-fift test-cells test-smartcont test-net test-tdactor test-tdutils \
+      test-tonlib-offline test-adnl test-dht test-rldp test-rldp2 test-catchain \
+      test-fec test-tddb test-db test-validator-session-state
+else
 ninja storage-daemon storage-daemon-cli fift func tonlib tonlibjson tonlib-cli \
       validator-engine lite-client pow-miner validator-engine-console \
       generate-random-id json2tlo dht-server http-proxy rldp-http-proxy \
       adnl-proxy create-state emulator
+fi
 
 test $? -eq 0 || { echo "Can't compile ton"; exit 1; }
