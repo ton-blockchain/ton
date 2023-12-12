d307e2e92e06177c26bf1a468ed1bd1ce586ba52
neodiX <neodix@ton.org>
2023-12-12 15:50:32 +0100

    exclude some unstable tests

--- assembly/native/build-ubuntu-20.04-portable.sh
+++ assembly/native/build-ubuntu-20.04-portable.sh
@@ -206,5 +206,5 @@ fi
 
 if [ "$with_tests" = true ]; then
   cd build
-  ctest --output-on-failure -E "test-catchain|test-actors|test-smartcont|test-adnl|test-validator-session-state|test-dht"
+  ctest --output-on-failure -E "test-catchain|test-actors|test-smartcont|test-adnl|test-validator-session-state|test-dht|test-rldp"
 fi
--- assembly/native/build-ubuntu-20.04-shared.sh
+++ assembly/native/build-ubuntu-20.04-shared.sh
@@ -126,5 +126,5 @@ fi
 
 if [ "$with_tests" = true ]; then
   cd build
-  ctest --output-on-failure -E "test-catchain|test-actors|test-smartcont|test-adnl|test-validator-session-state"
+  ctest --output-on-failure -E "test-catchain|test-actors|test-smartcont|test-adnl|test-validator-session-state|test-dht|test-rldp"
 fi
