01b43acd8558430a2efd1910c3cb04956ad36424
neodiX <neodix@ton.org>
2023-12-17 23:07:14 +0100

    WIP jenkins pipeline changes

--- assembly/cicd/jenkins/test-builds.groovy
+++ assembly/cicd/jenkins/test-builds.groovy
@@ -8,11 +8,14 @@ pipeline {
                         label 'Ubuntu_x86-64'
                     }
                     steps {
-                        sh '''sudo apt-get update
-                            sudo apt-get install -y build-essential git openssl cmake ninja-build zlib1g-dev libssl-dev libsecp256k1-dev libmicrohttpd-dev libsodium-dev
-                            wget https://apt.llvm.org/llvm.sh
-                            chmod +x llvm.sh
-                            sudo ./llvm.sh 16 all
+                        /*
+                        sudo apt-get update
+                        sudo apt-get install -y build-essential git openssl cmake ninja-build zlib1g-dev libssl-dev libsecp256k1-dev libmicrohttpd-dev libsodium-dev
+                        wget https://apt.llvm.org/llvm.sh
+                        chmod +x llvm.sh
+                        sudo ./llvm.sh 16 all
+                        */
+                        sh '''
                             cp assembly/native/build-ubuntu-20.04-shared.sh .
                             chmod +x build-ubuntu-20.04-shared.sh
                             ./build-ubuntu-20.04-shared.sh -t -a
@@ -20,7 +23,24 @@ pipeline {
                     }
                     post {
                         always {
-                            archiveArtifacts artifacts: 'artifacts/**/*.*'
+                            archiveArtifacts artifacts: 'artifacts/*'
+                        }
+                    }
+                }
+                stage('Ubuntu 20.04 x86-64 (portable)') {
+                    agent {
+                        label 'Ubuntu_x86-64'
+                    }
+                    steps {
+                        sh '''
+                            cp assembly/native/build-ubuntu-20.04-portable.sh .
+                            chmod +x build-ubuntu-20.04-portable.sh
+                            ./build-ubuntu-20.04-portable.sh -t -a
+                           '''
+                    }
+                    post {
+                        always {
+                            archiveArtifacts artifacts: 'artifacts/*'
                         }
                     }
                 }
@@ -29,11 +49,7 @@ pipeline {
                         label 'Ubuntu_arm64'
                     }
                     steps {
-                        sh '''sudo apt-get update
-                            sudo apt-get install -y build-essential git openssl cmake ninja-build zlib1g-dev libssl-dev libsecp256k1-dev libmicrohttpd-dev libsodium-dev
-                            wget https://apt.llvm.org/llvm.sh
-                            chmod +x llvm.sh
-                            sudo ./llvm.sh 16 all
+                        sh '''
                             cp assembly/native/build-ubuntu-20.04-shared.sh .
                             chmod +x build-ubuntu-20.04-shared.sh
                             ./build-ubuntu-20.04-shared.sh -t -a
@@ -41,46 +57,126 @@ pipeline {
                     }
                     post {
                         always {
-                            archiveArtifacts artifacts: 'artifacts/**/*.*'
+                            archiveArtifacts artifacts: 'artifacts/*'
                         }
                     }
                 }
-                stage('macOS 12.7 x86-64') {
+                stage('Ubuntu 20.04 arm64 (portable)') {
+                    agent {
+                        label 'Ubuntu_arm64'
+                    }
+                    steps {
+                        sh '''
+                            cp assembly/native/build-ubuntu-20.04-portable.sh .
+                            chmod +x build-ubuntu-20.04-portable.sh
+                            ./build-ubuntu-20.04-portable.sh -t -a
+                           '''
+                    }
+                    post {
+                        always {
+                            archiveArtifacts artifacts: 'artifacts/*'
+                        }
+                    }
+                }
+                stage('macOS 12.7 x86-64 (shared)') {
                     agent {
                         label 'macOS_12.7_x86-64'
                     }
                     steps {
-                        sh 'pwd; ls; sw_vers; touch c; git branch'
+                        sh '''
+                            cp assembly/native/build-macos-shared.sh .
+                            chmod +x build-macos-shared.sh
+                            ./build-macos-shared.sh -t -a
+                           '''
                     }
                     post {
                         always {
-                            archiveArtifacts artifacts: 'artifacts/**/*.*'
+                            archiveArtifacts artifacts: 'artifacts/*'
                         }
                     }
                 }
-                stage('macOS 12.6 arm64 (M1)') {
+                stage('macOS 12.7 x86-64 (portable)') {
+                    agent {
+                        label 'macOS_12.7_x86-64'
+                    }
+                    steps {
+                        sh '''
+                            cp assembly/native/build-macos-portable.sh .
+                            chmod +x build-macos-portable.sh
+                            ./build-macos-portable.sh -t -a -o 12.7
+                           '''
+                    }
+                    post {
+                        always {
+                            archiveArtifacts artifacts: 'artifacts/*'
+                        }
+                    }
+                }
+                stage('macOS 12.6 arm64 (M1, shared)') {
                     agent {
                         label 'macOS_12.6.3-arm64'
                     }
                     steps {
-                        sh 'pwd; ls; sw_vers; touch 4; git branch'
+                        sh '''
+                            cp assembly/native/build-macos-shared.sh .
+                            chmod +x build-macos-shared.sh
+                            ./build-macos-shared.sh -t -a
+                           '''
                     }
                     post {
                         always {
-                            archiveArtifacts artifacts: 'artifacts/**/*.*'
+                            archiveArtifacts artifacts: 'artifacts/*'
                         }
                     }
                 }
-                stage('macOS 13.2 arm64 (M2)') {
+                stage('macOS 12.6 arm64 (M1, portable)') {
+                    agent {
+                        label 'macOS_12.6.3-arm64'
+                    }
+                    steps {
+                        sh '''
+                            cp assembly/native/build-macos-portable.sh .
+                            chmod +x build-macos-portable.sh
+                            ./build-macos-portable.sh -t -a -o 12.6
+                           '''
+                    }
+                    post {
+                        always {
+                            archiveArtifacts artifacts: 'artifacts/*'
+                        }
+                    }
+                }
+                stage('macOS 13.2 arm64 (M2, shared)') {
                     agent {
                         label 'macOS_13.2-arm64-m2'
                     }
                     steps {
-                        sh 'pwd; ls; sw_vers; touch e; git branch'
+                        sh '''
+                            cp assembly/native/build-macos-shared.sh .
+                            chmod +x build-macos-shared.sh
+                            ./build-macos-shared.sh -t -a
+                           '''
                     }
                     post {
                         always {
-                            archiveArtifacts artifacts: 'artifacts/**/*.*'
+                            archiveArtifacts artifacts: 'artifacts/*'
+                        }
+                    }
+                }
+                stage('macOS 13.2 arm64 (M2, portable)') {
+                    agent {
+                        label 'macOS_13.2-arm64-m2'
+                    }
+                    steps {
+                        sh '''
+                            cp assembly/native/build-macos-portable.sh .
+                            chmod +x build-macos-portable.sh
+                            ./build-macos-portable.sh -t -a -o 13.2
+                           '''
+                    }
+                    post {
+                        always {
+                            archiveArtifacts artifacts: 'artifacts/*'
                         }
                     }
                 }
@@ -89,12 +185,16 @@ pipeline {
                         label 'Windows_x86-64'
                     }
                     steps {
-                        bat 'echo %cd%'
+                        bat '''
+                            copy assembly\\native\\build-windows-github.bat .
+                            copy assembly\\native\\build-windows.bat .
+                            build-windows-github.bat
+                            '''
                         bat 'git branch'
                     }
                     post {
                         always {
-                            archiveArtifacts artifacts: 'artifacts/**/*.*'
+                            archiveArtifacts artifacts: 'artifacts\\*'
                         }
                     }
                 }
