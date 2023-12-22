32492b5ecf9d096d4e06524ab0948326e0c8a99a
neodiX <neodix@ton.org>
2023-12-22 14:38:37 +0100

    WIP jenkins pipeline: include mac nix build

--- assembly/cicd/jenkins/test-builds.groovy
+++ assembly/cicd/jenkins/test-builds.groovy
@@ -71,7 +71,8 @@ pipeline {
                     steps {
                         timeout(time: 90, unit: 'MINUTES') {
                             sh '''
-                            cp assembly/nix/linux-x86-64* .
+                            cp assembly/nix/linux-x86-64-static.nix .
+                            cp assembly/nix/linux-arm64-tonlib.nix .
                             export NIX_PATH=nixpkgs=https://github.com/nixOS/nixpkgs/archive/23.11.tar.gz
                             nix-build linux-x86-64-static.nix
                             mkdir tmp
@@ -163,22 +164,6 @@ pipeline {
                             '''
                             sh 'zip -r ton-arm64-m2-macos-shared ./artifacts/*'
                             archiveArtifacts artifacts: 'ton-arm64-m2-macos-shared.zip'
-                        }
-                    }
-                }
-                stage('macOS 13.2 aarch64 (portable)') {
-                    agent {
-                        label 'macOS_13.2-arm64-m2'
-                    }
-                    steps {
-                        timeout(time: 90, unit: 'MINUTES') {
-                            sh '''
-                            cp assembly/nix/macos-arm64-* .
-                            export NIX_PATH=nixpkgs=https://github.com/nixOS/nixpkgs/archive/23.11.tar.gz
-                            nix-build macos-arm64-static.nix
-                            '''
-                            sh 'zip -r ton-arm64-m2-macos-portable ./result/bin/*'
-                            archiveArtifacts artifacts: 'ton-arm64-m2-macos-portable.zip'
                         }
                     }
                 }
