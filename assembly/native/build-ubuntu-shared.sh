b13d565c756fb39860a697b58ef33c20f0c1e3d7
neodiX <neodix@ton.org>
2024-02-11 21:17:51 +0100

    2-steps cross compile on Ubuntu x86-64 for aarch64

--- assembly/native/build-ubuntu-shared.sh
+++ assembly/native/build-ubuntu-shared.sh
@@ -11,6 +11,8 @@ while getopts 'ta' flag; do
   case "${flag}" in
     t) with_tests=true ;;
     a) with_artifacts=true ;;
+    p) prepare_cross=true ;;
+    s) no_strip=true ;;
     *) break
        ;;
   esac
@@ -61,13 +63,18 @@ ninja storage-daemon storage-daemon-cli fift func tonlib tonlibjson tonlib-cli \
       test-fec test-tddb test-db test-validator-session-state
       test $? -eq 0 || { echo "Can't compile ton"; exit 1; }
 else
-ninja storage-daemon storage-daemon-cli fift func tonlib tonlibjson tonlib-cli \
-      validator-engine lite-client pow-miner validator-engine-console blockchain-explorer \
-      generate-random-id json2tlo dht-server http-proxy rldp-http-proxy \
-      adnl-proxy create-state emulator
-      test $? -eq 0 || { echo "Can't compile ton"; exit 1; }
+    if [ "$prepare_cross" = true ]; then
+      ninja prepare_cross_compiling storage-daemon
+    else
+      ninja storage-daemon storage-daemon-cli fift func tonlib tonlibjson tonlib-cli \
+            validator-engine lite-client pow-miner validator-engine-console blockchain-explorer \
+            generate-random-id json2tlo dht-server http-proxy rldp-http-proxy \
+            adnl-proxy create-state emulator
+            test $? -eq 0 || { echo "Can't compile ton"; exit 1; }
+      fi
 fi
 
+if [ "$no_strip" = false ]; then
 strip -g storage/storage-daemon/storage-daemon \
          storage/storage-daemon/storage-daemon-cli \
          blockchain-explorer/blockchain-explorer \
@@ -87,6 +94,7 @@ strip -g storage/storage-daemon/storage-daemon \
          utils/json2tlo \
          adnl/adnl-proxy \
          emulator/libemulator.*
+fi
 
 test $? -eq 0 || { echo "Can't strip final binaries"; exit 1; }
 
---
+++ assembly/nix/linux-cross-x86_64-aarch64-static.nix
@@ -0,0 +1,40 @@
+let
+  nixpkgs = fetchTarball "https://github.com/nixOS/nixpkgs/archive/23.05.tar.gz";
+  pkgs = (import nixpkgs {}).pkgsCross.aarch64-unknown-linux-gnu;
+in
+pkgs.callPackage (
+    { mkShell, cmake, ninja, git, pkg-config  }:
+  pkgs.stdenv.mkDerivation {
+  pname = "ton";
+  version = "dev-bin";
+
+  src = ./.;
+
+  nativeBuildInputs = with pkgs;
+    [
+      cmake ninja git pkg-config
+    ];
+
+  buildInputs = with pkgs;
+    [
+      pkgsStatic.openssl pkgsStatic.libmicrohttpd pkgsStatic.zlib pkgsStatic.libsodium.dev pkgsStatic.secp256k1 glibc.static
+    ];
+
+  makeStatic = true;
+#  doCheck = testing;
+
+  cmakeFlags = [
+    "-DTON_USE_ABSEIL=OFF"
+    "-DNIX=ON"
+    "-DBUILD_SHARED_LIBS=OFF"
+    "-DCMAKE_LINK_SEARCH_START_STATIC=ON"
+    "-DCMAKE_LINK_SEARCH_END_STATIC=ON"
+    "-DCMAKE_CTEST_ARGUMENTS=--timeout;1800"
+    "-DCMAKE_SYSTEM_NAME=Linux"
+    "-DCMAKE_CROSSCOMPILING=TRUE"
+  ];
+
+  LDFLAGS = [
+     "-static-libgcc" "-static-libstdc++" "-static"
+  ];
+}) {}
