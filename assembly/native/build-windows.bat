24ce21c20e1e64a6ecd49afe51966ce48ab4e205
My Name <my-name@chromium.org>
2024-02-29 15:17:21 +0100

    exclude test-bigint execution on windows

--- assembly/native/build-windows.bat
+++ assembly/native/build-windows.bat
@@ -166,7 +166,7 @@ IF %errorlevel% NEQ 0 (
 IF "%1"=="-t" (
   echo Running tests...
 REM  ctest -C Release --output-on-failure -E "test-catchain|test-actors|test-validator-session-state"
-  ctest -C Release --output-on-failure --timeout 1800
+  ctest -C Release --output-on-failure -E "test-bigint" --timeout 1800
   IF %errorlevel% NEQ 0 (
     echo Some tests failed
     exit /b %errorlevel%
