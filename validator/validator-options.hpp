e985ac03586e1e578e25b2b7d4cdec3c8990f184
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2024-08-07 14:25:45 +0300

    Add --fast-state-serializer flag and responding to AdnlMessageCreateChannel with Nop (#1096)
    * Add --fast-state-serializer and tools for jemalloc
    
    * Disable fast state serializer by default unless RAM is >= 90GB
    * Print jemalloc stats once a minute
    * Dump jemalloc profile on request
    
    * Respond to AdnlMessageCreateChannel with Nop
    
    ---------
    
    Co-authored-by: SpyCheese <mikle98@yandex.ru>

--- validator/validator-options.hpp
+++ validator/validator-options.hpp
@@ -147,6 +147,9 @@ struct ValidatorManagerOptionsImpl : public ValidatorManagerOptions {
   td::Ref<CollatorOptions> get_collator_options() const override {
     return collator_options_;
   }
+  bool get_fast_state_serializer_enabled() const override {
+    return fast_state_serializer_enabled_;
+  }
 
   void set_zero_block_id(BlockIdExt block_id) override {
     zero_block_id_ = block_id;
@@ -233,6 +236,9 @@ struct ValidatorManagerOptionsImpl : public ValidatorManagerOptions {
   void set_collator_options(td::Ref<CollatorOptions> value) override {
     collator_options_ = std::move(value);
   }
+  void set_fast_state_serializer_enabled(bool value) override {
+    fast_state_serializer_enabled_ = value;
+  }
 
   ValidatorManagerOptionsImpl *make_copy() const override {
     return new ValidatorManagerOptionsImpl(*this);
@@ -286,6 +292,7 @@ struct ValidatorManagerOptionsImpl : public ValidatorManagerOptions {
   td::optional<double> catchain_max_block_delay_;
   bool state_serializer_enabled_ = true;
   td::Ref<CollatorOptions> collator_options_{true};
+  bool fast_state_serializer_enabled_ = false;
 };
 
 }  // namespace validator
