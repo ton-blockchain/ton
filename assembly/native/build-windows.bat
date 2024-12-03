844b168ba679670631f1a30b47faab16fbe4c6f0
neodiX <neodix@ton.org>
2024-12-03 20:31:46 +0400

    install nasm for openssl compilation on win for github

--- assembly/native/build-windows.bat
+++ assembly/native/build-windows.bat
@@ -28,6 +28,8 @@ IF %errorlevel% NEQ 0 (
 
 echo Installing nasm...
 choco install -y nasm
+where nasm
+SET PATH=%PATH%;C:\Program Files\NASM
 IF %errorlevel% NEQ 0 (
   echo Can't install nasm
   exit /b %errorlevel%
