9359f9d65eed213c96720b44df0069255a8fc1e3
olegvallas <oleg.vallas@gmail.com>
2025-10-13 02:40:20 +0200

    Activate improved compression.

--- validator/collator-node/utils.cpp
+++ validator/collator-node/utils.cpp
@@ -29,12 +29,10 @@ tl_object_ptr<ton_api::collatorNode_Candidate> serialize_candidate(const BlockCa
         PublicKey{pubkeys::Ed25519{block.pubkey.as_bits256()}}.tl(), create_tl_block_id(block.id), block.data.clone(),
         block.collated_data.clone());
   }
-  size_t decompressed_size;
   td::BufferSlice compressed =
-      validatorsession::compress_candidate_data(block.data, block.collated_data, decompressed_size).move_as_ok();
-  return create_tl_object<ton_api::collatorNode_compressedCandidate>(
-      0, PublicKey{pubkeys::Ed25519{block.pubkey.as_bits256()}}.tl(), create_tl_block_id(block.id),
-      (int)decompressed_size, std::move(compressed));
+      validatorsession::compress_candidate_data(block.data, block.collated_data).move_as_ok();
+  return create_tl_object<ton_api::collatorNode_compressedCandidateV2>(
+      0, PublicKey{pubkeys::Ed25519{block.pubkey.as_bits256()}}.tl(), create_tl_block_id(block.id), std::move(compressed));
 }
 
 td::Result<BlockCandidate> deserialize_candidate(tl_object_ptr<ton_api::collatorNode_Candidate> f,
