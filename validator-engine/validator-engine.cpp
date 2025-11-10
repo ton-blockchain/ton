fef06fe9413f8512a4e33713d9332712fa07391e
SpyCheese <mikle98@yandex.ru>
2025-11-10 17:12:32 +0300

    Always enable celldb-v2

--- validator-engine/validator-engine.cpp
+++ validator-engine/validator-engine.cpp
@@ -1544,9 +1544,6 @@ td::Status ValidatorEngine::load_global_config() {
   if (zero_state.root_hash.is_zero() || zero_state.file_hash.is_zero()) {
     return td::Status::Error(ton::ErrorCode::error, "[validator] section contains incomplete [zero_state]");
   }
-  if (celldb_in_memory_ && celldb_v2_) {
-    return td::Status::Error(ton::ErrorCode::error, "at most one of --celldb-in-memory --celldb-v2 could be used");
-  }
 
   ton::BlockIdExt init_block;
   if (!conf.validator_->init_block_) {
@@ -1599,7 +1596,7 @@ td::Status ValidatorEngine::load_global_config() {
   }
   validator_options_.write().set_celldb_compress_depth(celldb_compress_depth_);
   validator_options_.write().set_celldb_in_memory(celldb_in_memory_);
-  validator_options_.write().set_celldb_v2(celldb_v2_);
+  validator_options_.write().set_celldb_v2(!celldb_in_memory_);
   validator_options_.write().set_celldb_disable_bloom_filter(celldb_disable_bloom_filter_);
   validator_options_.write().set_max_open_archive_files(max_open_archive_files_);
   validator_options_.write().set_archive_preload_period(archive_preload_period_);
@@ -5459,8 +5456,7 @@ int main(int argc, char *argv[]) {
       '\0', "celldb-in-memory",
       "store all cells in-memory, much faster but requires a lot of RAM. RocksDb is still used as persistent storage",
       [&]() { acts.push_back([&x]() { td::actor::send_closure(x, &ValidatorEngine::set_celldb_in_memory, true); }); });
-  p.add_option('\0', "celldb-v2", "use new version off celldb", [&]() {
-    acts.push_back([&x]() { td::actor::send_closure(x, &ValidatorEngine::set_celldb_v2, true); });
+  p.add_option('\0', "celldb-v2", "deprecated option (enabled by default)", [&]() {
   });
   p.add_option(
       '\0', "celldb-disable-bloom-filter",
