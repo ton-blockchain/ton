31cf815310e6ac099747f06ce98873f9dfe2b8d0
Dan Klishch <danilklishch@gmail.com>
2025-11-10 22:38:02 -0500

    [tontester] Add abstract Network.Node::run method

--- test/tontester/src/tontester/network.py
+++ test/tontester/src/tontester/network.py
@@ -4,6 +4,7 @@ import logging
 import subprocess
 import time
 import types
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import IntEnum, auto
 from ipaddress import IPv4Address
@@ -40,7 +41,7 @@ def _write_model(file: Path, model: BaseModel):
 
 @final
 class Network:
-    class Node:
+    class Node(ABC):
         name: str
 
         _network: "Network"
@@ -125,6 +126,10 @@ class Network:
 
         def announce_to(self, dht: "DHTNode"):
             self._static_nodes.append(dht)
+
+        @abstractmethod
+        async def run(self):
+            pass
 
         async def stop(self):
             if self.__process:
@@ -279,6 +284,7 @@ class DHTNode(Network.Node):
     def signed_address(self):
         return self._signed_address
 
+    @override
     async def run(self):
         await self._run(self._install.dht_server_exe, self._local_config, None, [])
 
@@ -356,6 +362,7 @@ class FullNode(Network.Node):
     def validator_key(self):
         return self._validator_key
 
+    @override
     async def run(self):
         zerostate = self._get_or_generate_zerostate()
 
