3507bcb8575b1e3c2c6cab1a70082c2fdfc5d4a8
neodix <namlem@gmail.com>
2025-12-02 16:33:30 +0400

    minor

--- assembly/native/build-ubuntu-portable.sh
+++ assembly/native/build-ubuntu-portable.sh
@@ -96,12 +96,12 @@ fi
 
 if [ ! -d "../3pp/libmicrohttpd" ]; then
   git clone https://github.com/neodix42/libmicrohttpd.git ../3pp/libmicrohttpd
-  cd ../3pp/libmicrohttpd
+  cd ../3pp/libmicrohttpd || exit
   libmicrohttpdPath=`pwd`
   ./configure --enable-static --disable-tests --disable-benchmark --disable-shared --disable-https --with-pic
   make -j$(nproc)
   test $? -eq 0 || { echo "Can't compile libmicrohttpd"; exit 1; }
-  cd ../../build
+  cd ../../build || exit
 else
   libmicrohttpdPath=$(pwd)/../3pp/libmicrohttpd
   echo "Using compiled libmicrohttpd"
