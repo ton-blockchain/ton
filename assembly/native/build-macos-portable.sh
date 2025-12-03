d469e37c66e35032eea32f330c88348fbd5b4e49
neodix42 <namlem@gmail.com>
2025-12-03 19:09:41 +0400

    fetch libmicrohttpd from github instead of ftpmirror.gnu.org (#1935)
    * fetch libmicrohttpd from github instead of ftpmirror.gnu.org
    
    * remove redundant snippet
    
    * remove redundant snippet
    
    * minor
    
    * use libmicrohttpd repo out of ton-blockchain org

--- assembly/native/build-macos-portable.sh
+++ assembly/native/build-macos-portable.sh
@@ -109,18 +109,15 @@ else
 fi
 
 if [ ! -d "../3pp/libmicrohttpd" ]; then
-  mkdir -p ../3pp/libmicrohttpd
-  wget -O ../3pp/libmicrohttpd/libmicrohttpd-1.0.1.tar.gz https://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-1.0.1.tar.gz
-  cd ../3pp/libmicrohttpd/
-  tar xf libmicrohttpd-1.0.1.tar.gz
-  cd libmicrohttpd-1.0.1
+  git clone https://github.com/ton-blockchain/libmicrohttpd.git ../3pp/libmicrohttpd
+  cd ../3pp/libmicrohttpd
   libmicrohttpdPath=`pwd`
   ./configure --enable-static --disable-tests --disable-benchmark --disable-shared --disable-https --with-pic
   make -j4
   test $? -eq 0 || { echo "Can't compile libmicrohttpd"; exit 1; }
-  cd ../../../build
+  cd ../../build
 else
-  libmicrohttpdPath=$(pwd)/../3pp/libmicrohttpd/libmicrohttpd-1.0.1
+  libmicrohttpdPath=$(pwd)/../3pp/libmicrohttpd
   echo "Using compiled libmicrohttpd"
 fi
 
