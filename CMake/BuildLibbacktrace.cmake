2094284ccd3d279bb1b1c58a60fadf7597329297
birydrad <>
2025-12-19 22:52:03 +0000

    add libbacktrace as submodule

--- CMake/BuildLibbacktrace.cmake
+++ CMake/BuildLibbacktrace.cmake
@@ -5,11 +5,16 @@
 #   1. Use pre-built library:
 #      set(LIBBACKTRACE_LIBRARY "/path/to/libbacktrace.a")
 #      set(LIBBACKTRACE_INCLUDE_DIR "/path/to/include")
-#   2. Build from submodule:
-#      set(LIBBACKTRACE_SOURCE_DIR "/path/to/libbacktrace/source")
-#   3. Auto-download from GitHub (default if nothing is set)
+#   2. Build from submodule (default): uses third-party/libbacktrace
 
 include(ExternalProject)
+
+# Skip libbacktrace on Android - it doesn't work well with Android's unwinder
+if(ANDROID)
+  message(STATUS "Skipping libbacktrace on Android (not supported)")
+  set(LIBBACKTRACE_FOUND FALSE CACHE BOOL "libbacktrace found" FORCE)
+  return()
+endif()
 
 # Check if user provided pre-built library
 if(LIBBACKTRACE_LIBRARY AND LIBBACKTRACE_INCLUDE_DIR)
@@ -42,46 +47,54 @@ endif()
 file(MAKE_DIRECTORY "${LIBBACKTRACE_INCLUDE_DIR}")
 file(MAKE_DIRECTORY "${LIBBACKTRACE_ROOT_DIR}/lib")
 
-# Check if source path is provided (e.g., git submodule)
-if(LIBBACKTRACE_SOURCE_DIR)
-  message(STATUS "Building libbacktrace from: ${LIBBACKTRACE_SOURCE_DIR}")
-  
-  ExternalProject_Add(
-    libbacktrace_external
-    PREFIX "${LIBBACKTRACE_ROOT_DIR}"
-    SOURCE_DIR "${LIBBACKTRACE_SOURCE_DIR}"
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env
-      CC=${CMAKE_C_COMPILER}
-      <SOURCE_DIR>/configure
-      --prefix=<INSTALL_DIR>
-      --enable-static
-      --disable-shared
-      --with-pic
-    BUILD_COMMAND make
-    INSTALL_COMMAND make install
-    BUILD_BYPRODUCTS "${LIBBACKTRACE_LIBRARY}"
-  )
-else()
-  message(STATUS "Building libbacktrace from GitHub")
-  
-  ExternalProject_Add(
-    libbacktrace_external
-    PREFIX "${LIBBACKTRACE_ROOT_DIR}"
-    GIT_REPOSITORY https://github.com/ianlancetaylor/libbacktrace.git
-    GIT_TAG b9e40069c0b47a722286b94eb5231f7f05c08713
-    GIT_SHALLOW TRUE
-    CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env
-      CC=${CMAKE_C_COMPILER}
-      <SOURCE_DIR>/configure
-      --prefix=<INSTALL_DIR>
-      --enable-static
-      --disable-shared
-      --with-pic
-    BUILD_COMMAND make
-    INSTALL_COMMAND make install
-    BUILD_BYPRODUCTS "${LIBBACKTRACE_LIBRARY}"
-  )
+# Use submodule path if not overridden
+if(NOT LIBBACKTRACE_SOURCE_DIR)
+  set(LIBBACKTRACE_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third-party/libbacktrace")
 endif()
+
+message(STATUS "Building libbacktrace from: ${LIBBACKTRACE_SOURCE_DIR}")
+
+# Build configure arguments - handle cross-compilation
+set(LIBBACKTRACE_CONFIGURE_ARGS
+  --prefix=<INSTALL_DIR>
+  --enable-static
+  --disable-shared
+  --with-pic
+)
+
+# For cross-compilation, pass the host triplet
+if(CMAKE_CROSSCOMPILING AND CMAKE_C_COMPILER_TARGET)
+  list(APPEND LIBBACKTRACE_CONFIGURE_ARGS --host=${CMAKE_C_COMPILER_TARGET})
+endif()
+
+# Set up environment for configure
+set(LIBBACKTRACE_ENV
+  CC=${CMAKE_C_COMPILER}
+)
+
+# For cross-compilation, also set AR and RANLIB if available
+if(CMAKE_AR)
+  list(APPEND LIBBACKTRACE_ENV AR=${CMAKE_AR})
+endif()
+if(CMAKE_RANLIB)
+  list(APPEND LIBBACKTRACE_ENV RANLIB=${CMAKE_RANLIB})
+endif()
+
+# Pass C flags (important for cross-compilation sysroot, etc.)
+if(CMAKE_C_FLAGS)
+  list(APPEND LIBBACKTRACE_ENV "CFLAGS=${CMAKE_C_FLAGS}")
+endif()
+
+ExternalProject_Add(
+  libbacktrace_external
+  PREFIX "${LIBBACKTRACE_ROOT_DIR}"
+  SOURCE_DIR "${LIBBACKTRACE_SOURCE_DIR}"
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env ${LIBBACKTRACE_ENV}
+    <SOURCE_DIR>/configure ${LIBBACKTRACE_CONFIGURE_ARGS}
+  BUILD_COMMAND make
+  INSTALL_COMMAND make install
+  BUILD_BYPRODUCTS "${LIBBACKTRACE_LIBRARY}"
+)
 
 # Create imported target
 add_library(libbacktrace STATIC IMPORTED GLOBAL)
