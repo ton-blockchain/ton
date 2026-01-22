0a8c556ed682c0ad6a53a970357b898ce158c1c3
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2026-01-22 10:29:54 +0300

    write_config via atomic_write_file

--- validator-engine/validator-engine.cpp
+++ validator-engine/validator-engine.cpp
@@ -2022,14 +2022,12 @@ void ValidatorEngine::load_config(td::Promise<td::Unit> promise) {
 void ValidatorEngine::write_config(td::Promise<td::Unit> promise) {
   auto s = td::json_encode<std::string>(td::ToJson(*config_.tl().get()), true);
 
-  auto S = td::write_file(temp_config_file(), s);
+  auto S = td::atomic_write_file(config_file_, s, temp_config_file());
   if (S.is_error()) {
     td::unlink(temp_config_file()).ignore();
     promise.set_error(std::move(S));
     return;
   }
-  td::unlink(config_file_).ignore();
-  TRY_STATUS_PROMISE(promise, td::rename(temp_config_file(), config_file_));
   promise.set_value(td::Unit());
 }
 
