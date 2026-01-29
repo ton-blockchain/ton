5d5a2fb2b8b75534d005b3c0fe2586c45b8c3e38
Dan Klishch <danilklishch@gmail.com>
2026-01-29 01:22:54 -0500

    Remove td::AtomicRead and replace its single use with std::mutex (#2105)
    AtomicRead is a source of CI flakes on MacOS because it has a data race.
    There is no point in trying to invent a seqlock for a single place in
    the codebase, so let's just remove it.

--- tdactor/td/actor/core/Scheduler.h
+++ tdactor/td/actor/core/Scheduler.h
@@ -36,7 +36,6 @@
 #include "td/actor/core/SchedulerContext.h"
 #include "td/actor/core/SchedulerId.h"
 #include "td/actor/core/SchedulerMessage.h"
-#include "td/utils/AtomicRead.h"
 #include "td/utils/Closure.h"
 #include "td/utils/Heap.h"
 #include "td/utils/List.h"
@@ -84,13 +83,15 @@ struct Debug {
     return need_debug();
   }
   struct Destructor {
-    void operator()(Debug *info) {
-      info->info_.lock().value().is_active = false;
+    void operator()(Debug *debug) {
+      std::lock_guard<std::mutex> lock(debug->info_mutex_);
+      debug->info_.is_active = false;
     }
   };
 
   void read(DebugInfo &info) {
-    info_.read(info);
+    std::lock_guard<std::mutex> lock(info_mutex_);
+    info = info_;
   }
 
   std::unique_ptr<Debug, Destructor> start(td::Slice name) {
@@ -98,17 +99,17 @@ struct Debug {
       return {};
     }
     {
-      auto lock = info_.lock();
-      auto &value = lock.value();
-      value.is_active = true;
-      value.start_at = Time::now();
-      value.set_name(name);
+      std::lock_guard<std::mutex> lock(info_mutex_);
+      info_.is_active = true;
+      info_.start_at = Time::now();
+      info_.set_name(name);
     }
     return std::unique_ptr<Debug, Destructor>(this);
   }
 
  private:
-  AtomicRead<DebugInfo> info_;
+  std::mutex info_mutex_;
+  DebugInfo info_;
 };
 
 struct WorkerInfo {
