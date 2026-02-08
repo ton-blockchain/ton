b457a05cbbd9b885c27e98520fa6efcdc9891176
SpyCheese <mikle98@yandex.ru>
2026-02-08 20:53:28 +0300

    Changes in block producer

--- tdactor/td/actor/SharedFuture.h
+++ tdactor/td/actor/SharedFuture.h
@@ -7,6 +7,9 @@
 #pragma once
 
 #include "td/actor/coro_task.h"
+#include "td/utils/CancellationToken.h"
+
+#include "coro_utils.h"
 
 namespace td::actor {
 
@@ -15,41 +18,29 @@ class SharedFuture {
  public:
   SharedFuture() = default;
 
-  SharedFuture(td::actor::StartedTask<T> future) : future_(std::move(future)) {
+  SharedFuture(StartedTask<T> future) : future_(std::move(future)) {
   }
 
   SharedFuture(SharedFuture&&) = default;
   SharedFuture& operator=(SharedFuture&&) = default;
 
-  td::actor::Task<T> get() {
+  Task<T> get() {
     if (is_resolved) {
-      if (value_.has_value()) {
-        co_return *value_;
-      } else {
-        co_return get_error();
-      }
+      co_return value_.clone();
     }
 
-    auto [awaiter, promise] = td::actor::StartedTask<T>::make_bridge();
+    auto [awaiter, promise] = StartedTask<T>::make_bridge();
     promises_.push_back(std::move(promise));
 
     if (promises_.size() == 1) {
       CHECK(future_.valid());
-      td::Result<T> result = co_await std::move(future_).wrap();
-
+      auto token = cancellation_.get_cancellation_token();
+      auto result = co_await std::move(future_).wrap();
+      co_await token.check();
+      value_ = std::move(result);
       is_resolved = true;
-
-      if (result.is_error()) {
-        promises_[0].set_error(result.move_as_error());
-        for (size_t i = 1; i < promises_.size(); i++) {
-          promises_[i].set_error(get_error());
-        }
-      } else {
-        value_.emplace(result.move_as_ok());
-        for (auto& p : promises_) {
-          auto value_copy = *value_;
-          p.set_result(std::move(value_copy));
-        }
+      for (auto& p : promises_) {
+        p.set_result(value_.clone());
       }
     }
 
@@ -57,14 +48,33 @@ class SharedFuture {
   }
 
  private:
-  auto get_error() {
-    return td::Status::Error("SharedFuture<T> already resolved with error");
-  }
-
   bool is_resolved = false;
-  std::optional<T> value_;
-  td::actor::StartedTask<T> future_;
-  std::vector<td::Promise<T>> promises_;
+  Result<T> value_;
+  StartedTask<T> future_;
+  std::vector<Promise<T>> promises_;
+  CancellationTokenSource cancellation_;
 };
+
+constexpr int AWAIT_TIMEOUT_CODE = 6520;
+
+template <typename T>
+Task<T> await_with_timeout(Task<T> task, Timestamp timeout) {
+  auto [task_result, promise] = StartedTask<T>::make_bridge();
+  auto promise_ptr = std::make_shared<Promise<T>>(std::move(promise));
+  if (timeout) {
+    auto worker_timeout = [](Timestamp timeout, std::shared_ptr<Promise<T>> promise_ptr) -> Task<> {
+      co_await coro_sleep(timeout);
+      promise_ptr->set_error(Status::Error(AWAIT_TIMEOUT_CODE, "await timeout"));
+      co_return {};
+    };
+    worker_timeout(timeout, promise_ptr).start().detach();
+  }
+  auto worker_wait = [](Task<T> task, std::shared_ptr<Promise<T>> promise_ptr) -> Task<> {
+    promise_ptr->set_result(co_await std::move(task).wrap());
+    co_return {};
+  };
+  worker_wait(std::move(task), std::move(promise_ptr)).start().detach();
+  co_return co_await std::move(task_result);
+}
 
 }  // namespace td::actor
