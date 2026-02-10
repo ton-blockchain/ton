c53c62bef480a927c1e6f7b120ddb26b1c0d8e41
Vadim Averin <vadim@avevad.com>
2026-02-10 04:35:25 +0000

    Moar collectors

--- test/net/bench-rldp.cpp
+++ test/net/bench-rldp.cpp
@@ -369,9 +369,10 @@ void run_server(Config config) {
   td::actor::ActorOwn<ton::PrometheusExporter> exporter;
 
   scheduler.run_in_context([&] {
-    if (config.prometheus)
+    if (config.prometheus) {
       exporter = ton::PrometheusExporter::listen(19777);
-
+      td::actor::send_closure(exporter, &ton::PrometheusExporter::register_collector<ton::PrometheusExporter>, exporter.get());
+    }
     keyring = ton::keyring::Keyring::create(db_root);
     network_manager = ton::adnl::AdnlNetworkManager::create(static_cast<td::uint16>(config.local_addr.get_port()));
     adnl = ton::adnl::Adnl::create(db_root, keyring.get());
@@ -441,8 +442,10 @@ void run_client(Config config) {
   ton::adnl::AdnlNodeIdShort dst;
 
   scheduler.run_in_context([&] {
-    if (config.prometheus)
+    if (config.prometheus) {
       exporter = ton::PrometheusExporter::listen(29777);
+      td::actor::send_closure(exporter, &ton::PrometheusExporter::register_collector<ton::PrometheusExporter>, exporter.get());
+    }
 
     keyring = ton::keyring::Keyring::create(db_root);
     network_manager = ton::adnl::AdnlNetworkManager::create(static_cast<td::uint16>(config.local_addr.get_port()));
