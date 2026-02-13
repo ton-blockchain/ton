703ee6fcfb8a922dcbb878be31ca91cc8392f47f
EmelyanenkoK <emelyanenko.kirill@gmail.com>
2026-02-13 16:36:25 +0300

    Lower verbosity of the slow twostep broadcast logs. (#2133)
    Co-authored-by: olegvallas <oleg.vallas@gmail.com>

--- overlay/broadcast-twostep.cpp
+++ overlay/broadcast-twostep.cpp
@@ -46,7 +46,7 @@ namespace ton {
 namespace overlay {
 
 constexpr int VERBOSITY_NAME(TWOSTEP_WARNING) = verbosity_WARNING;
-constexpr int VERBOSITY_NAME(TWOSTEP_INFO) = verbosity_INFO;
+constexpr int VERBOSITY_NAME(TWOSTEP_INFO) = verbosity_DEBUG;
 constexpr int VERBOSITY_NAME(TWOSTEP_DEBUG) = verbosity_DEBUG;
 
 static constexpr std::size_t FEC_MIN_BYTES = 513;
