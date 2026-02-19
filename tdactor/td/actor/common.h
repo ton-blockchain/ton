0c68df3322ea11d52d4081b41153d10e098bc6c0
Dan Klishch <danilklishch@gmail.com>
2026-02-19 12:55:49 -0500

    Allow to return value from Scheduler::run_in_context

--- tdactor/td/actor/common.h
+++ tdactor/td/actor/common.h
@@ -168,8 +168,8 @@ class Scheduler {
   }
 
   template <class F>
-  void run_in_context(F &&f) {
-    schedulers_[0]->run_in_context(std::forward<F>(f));
+  auto run_in_context(F &&f) {
+    return schedulers_[0]->run_in_context(std::forward<F>(f));
   }
 
   void stop() {
