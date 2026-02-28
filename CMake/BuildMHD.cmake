ceefdd175045e6e254347b5ac6c62aecf4550436
neodix42 <namlem@gmail.com>
2026-02-28 09:35:01 +0400

    Rework third party builds (#2169)
    * retrigger builds
    
    * let third-parties use the build folder
    
    * improve emscripten build by using CMAKE_BINARY_DIR
    
    * adjust MSVC build paths
    
    * codex comment review: adjust OS
    
    * codex comment review: adjust OpenSSL emscripten build for better compatibility
    
    * Adjusted libmicrohttpd build to avoid autotools regeneration on macOS.
    
    * patch mhd and zlib build modules to make ranlib invocation portable

--- CMake/BuildMHD.cmake
+++ CMake/BuildMHD.cmake
@@ -6,6 +6,7 @@ set(TON_THIRD_PARTY_BINARY_DIR "${CMAKE_BINARY_DIR}/third-party")
 
 set(MHD_SOURCE_DIR ${TON_THIRD_PARTY_SOURCE_DIR}/libmicrohttpd)
 set(MHD_BINARY_DIR ${TON_THIRD_PARTY_BINARY_DIR}/libmicrohttpd)
+set(MHD_BUILD_DIR ${MHD_BINARY_DIR}/src)
 
 if (USE_EMSCRIPTEN OR EMSCRIPTEN)
   message(STATUS "libmicrohttpd is not built for emscripten")
@@ -31,15 +32,22 @@ elseif (MSVC)
   endif()
 
   set(MHD_PROJECT_DIR ${MHD_SOURCE_DIR}/w32/${MHD_MSVC_PROJECT_SUBDIR})
-  set(MHD_LIBRARY ${MHD_PROJECT_DIR}/Output/x64/libmicrohttpd.lib)
+  set(MHD_LIBRARY ${MHD_BINARY_DIR}/lib/libmicrohttpd.lib)
   set(MHD_INCLUDE_DIR ${MHD_SOURCE_DIR}/src/include)
+  file(MAKE_DIRECTORY ${MHD_BINARY_DIR}/lib)
+  file(MAKE_DIRECTORY ${MHD_BINARY_DIR}/obj)
+  set(MHD_MSVC_OUT_DIR "${MHD_BINARY_DIR}/lib/")
+  set(MHD_MSVC_INT_DIR "${MHD_BINARY_DIR}/obj/")
+
   set(MHD_MSBUILD_ARGS
-      libmicrohttpd.vcxproj
-      /p:Configuration=Release-static
-      /p:platform=x64
+    libmicrohttpd.vcxproj
+    /p:Configuration=Release-static
+    /p:platform=x64
+    /p:OutDir=${MHD_MSVC_OUT_DIR}
+    /p:IntDir=${MHD_MSVC_INT_DIR}
   )
   if (MHD_MSVC_TOOLSET)
-    list(APPEND MHD_MSBUILD_ARGS -p:PlatformToolset=${MHD_MSVC_TOOLSET})
+    list(APPEND MHD_MSBUILD_ARGS /p:PlatformToolset=${MHD_MSVC_TOOLSET})
   endif()
 
   if (NOT EXISTS "${MHD_LIBRARY}")
@@ -59,13 +67,16 @@ elseif (MSVC)
     DEPENDS ${MHD_SOURCE_DIR}
     OUTPUT ${MHD_LIBRARY}
   )
-elseif (ANDROID)
-  set(MHD_BINARY_DIR ${TON_ANDROID_THIRD_PARTY_DIR}/libmicrohttpd/${TON_ANDROID_ARCH_DIR})
+else()
+  if (ANDROID)
+    set(MHD_BINARY_DIR ${TON_ANDROID_THIRD_PARTY_DIR}/libmicrohttpd/${TON_ANDROID_ARCH_DIR})
+    set(MHD_BUILD_DIR ${MHD_BINARY_DIR}/src)
+  endif()
   set(MHD_LIBRARY ${MHD_BINARY_DIR}/lib/libmicrohttpd.a)
   set(MHD_INCLUDE_DIR ${MHD_BINARY_DIR}/include)
-
-  file(MAKE_DIRECTORY ${MHD_BINARY_DIR})
+  file(MAKE_DIRECTORY ${MHD_BINARY_DIR}/lib)
   file(MAKE_DIRECTORY ${MHD_INCLUDE_DIR})
+  file(MAKE_DIRECTORY ${MHD_BUILD_DIR})
 
   if (CMAKE_C_FLAGS)
     set(MHD_CFLAGS "${CMAKE_C_FLAGS} -fPIC")
@@ -73,81 +84,107 @@ elseif (ANDROID)
     set(MHD_CFLAGS "-fPIC")
   endif()
 
-  add_custom_command(
-      WORKING_DIRECTORY ${MHD_SOURCE_DIR}
-      COMMAND ${CMAKE_COMMAND} -E rm -f ${MHD_LIBRARY}
-      COMMAND ${CMAKE_COMMAND} -E env
-        CC=${TON_ANDROID_CC}
-        CXX=${TON_ANDROID_CXX}
-        AR=${TON_ANDROID_AR}
-        RANLIB=${TON_ANDROID_RANLIB}
-        CFLAGS=${MHD_CFLAGS}
-        ./configure --host=${TON_ANDROID_HOST} --prefix=${MHD_BINARY_DIR} --enable-static --disable-tests --disable-benchmark --disable-shared --disable-https --with-pic --disable-doc
-      COMMAND ${CMAKE_COMMAND} -E env
-        CC=${TON_ANDROID_CC}
-        CXX=${TON_ANDROID_CXX}
-        AR=${TON_ANDROID_AR}
-        RANLIB=${TON_ANDROID_RANLIB}
-        CFLAGS=${MHD_CFLAGS}
-        make clean
-      COMMAND ${CMAKE_COMMAND} -E env
-        CC=${TON_ANDROID_CC}
-        CXX=${TON_ANDROID_CXX}
-        AR=${TON_ANDROID_AR}
-        RANLIB=${TON_ANDROID_RANLIB}
-        CFLAGS=${MHD_CFLAGS}
-        make -j16
-      COMMAND ${CMAKE_COMMAND} -E env
-        CC=${TON_ANDROID_CC}
-        CXX=${TON_ANDROID_CXX}
-        AR=${TON_ANDROID_AR}
-        RANLIB=${TON_ANDROID_RANLIB}
-        CFLAGS=${MHD_CFLAGS}
-        make install
-      COMMAND ${TON_ANDROID_RANLIB} ${MHD_LIBRARY}
-      COMMENT "Build libmicrohttpd (Android)"
-      DEPENDS ${MHD_SOURCE_DIR}
-      OUTPUT ${MHD_LIBRARY}
+  set(MHD_CONFIGURE_ARGS
+    --prefix=${MHD_BINARY_DIR}
+    --enable-static
+    --disable-tests
+    --disable-benchmark
+    --disable-shared
+    --disable-https
+    --with-pic
+    --disable-doc
   )
-else()
-  set(MHD_INCLUDE_DIR ${MHD_SOURCE_DIR}/src/include)
-  set(MHD_LIBRARY ${MHD_SOURCE_DIR}/src/microhttpd/.libs/libmicrohttpd.a)
+  if (ANDROID)
+    list(APPEND MHD_CONFIGURE_ARGS --host=${TON_ANDROID_HOST})
+  endif()
 
   if (MINGW)
     find_program(MSYS2_BASH bash)
     if (NOT MSYS2_BASH)
       message(FATAL_ERROR "bash not found in PATH; ensure MSYS2 is in PATH.")
     endif()
+    string(REPLACE ";" " " MHD_CONFIGURE_ARGS_STR "${MHD_CONFIGURE_ARGS}")
     add_custom_command(
-        WORKING_DIRECTORY ${MHD_SOURCE_DIR}
-        COMMAND ${CMAKE_COMMAND} -E rm -f ${MHD_LIBRARY}
-        COMMAND ${CMAKE_COMMAND} -E touch ${MHD_SOURCE_DIR}/aclocal.m4
-        COMMAND ${MSYS2_BASH} -lc "./configure --enable-static --disable-tests --disable-benchmark --disable-shared --disable-https --with-pic --disable-doc"
-        COMMAND ${MSYS2_BASH} -lc "make clean ACLOCAL=: AUTOMAKE=: AUTOCONF=: AUTOHEADER=:"
-        COMMAND ${MSYS2_BASH} -lc "make -j16 ACLOCAL=: AUTOMAKE=: AUTOCONF=: AUTOHEADER=:"
-        COMMENT "Build libmicrohttpd"
-        DEPENDS ${MHD_SOURCE_DIR}
-        OUTPUT ${MHD_LIBRARY}
+      WORKING_DIRECTORY ${MHD_BINARY_DIR}
+      COMMAND ${CMAKE_COMMAND} -E rm -rf ${MHD_BUILD_DIR}
+      COMMAND ${CMAKE_COMMAND} -E copy_directory ${MHD_SOURCE_DIR} ${MHD_BUILD_DIR}
+      COMMAND ${MSYS2_BASH} -lc "cd '${MHD_BUILD_DIR}' && CC='${CMAKE_C_COMPILER}' CXX='${CMAKE_CXX_COMPILER}' AR='${CMAKE_AR}' RANLIB='${CMAKE_RANLIB}' CFLAGS='${MHD_CFLAGS}' ./configure ${MHD_CONFIGURE_ARGS_STR}"
+      COMMAND ${MSYS2_BASH} -lc "cd '${MHD_BUILD_DIR}' && CC='${CMAKE_C_COMPILER}' CXX='${CMAKE_CXX_COMPILER}' AR='${CMAKE_AR}' RANLIB='${CMAKE_RANLIB}' CFLAGS='${MHD_CFLAGS}' make clean ACLOCAL=: AUTOMAKE=: AUTOCONF=: AUTOHEADER=:"
+      COMMAND ${MSYS2_BASH} -lc "cd '${MHD_BUILD_DIR}' && CC='${CMAKE_C_COMPILER}' CXX='${CMAKE_CXX_COMPILER}' AR='${CMAKE_AR}' RANLIB='${CMAKE_RANLIB}' CFLAGS='${MHD_CFLAGS}' make -j16 ACLOCAL=: AUTOMAKE=: AUTOCONF=: AUTOHEADER=:"
+      COMMAND ${MSYS2_BASH} -lc "cd '${MHD_BUILD_DIR}' && CC='${CMAKE_C_COMPILER}' CXX='${CMAKE_CXX_COMPILER}' AR='${CMAKE_AR}' RANLIB='${CMAKE_RANLIB}' CFLAGS='${MHD_CFLAGS}' make install ACLOCAL=: AUTOMAKE=: AUTOCONF=: AUTOHEADER=:"
+      COMMENT "Build libmicrohttpd"
+      DEPENDS ${MHD_SOURCE_DIR}
+      OUTPUT ${MHD_LIBRARY}
     )
   else()
+    if (ANDROID)
+      set(MHD_CC ${TON_ANDROID_CC})
+      set(MHD_CXX ${TON_ANDROID_CXX})
+      set(MHD_AR ${TON_ANDROID_AR})
+      set(MHD_RANLIB ${TON_ANDROID_RANLIB})
+    else()
+      set(MHD_CC ${CMAKE_C_COMPILER})
+      set(MHD_CXX ${CMAKE_CXX_COMPILER})
+      set(MHD_AR ${CMAKE_AR})
+      set(MHD_RANLIB ${CMAKE_RANLIB})
+    endif()
+    if (NOT MHD_RANLIB)
+      find_program(MHD_RANLIB ranlib)
+    endif()
+    set(MHD_POST_INSTALL_COMMANDS)
+    if (MHD_RANLIB)
+      list(APPEND MHD_POST_INSTALL_COMMANDS COMMAND ${MHD_RANLIB} ${MHD_LIBRARY})
+    endif()
+
     add_custom_command(
-        WORKING_DIRECTORY ${MHD_SOURCE_DIR}
-        COMMAND ${CMAKE_COMMAND} -E rm -f ${MHD_LIBRARY}
-        COMMAND ${CMAKE_COMMAND} -E env
-          CC=${CMAKE_C_COMPILER}
-          CXX=${CMAKE_CXX_COMPILER}
-          ./configure --enable-static --disable-tests --disable-benchmark --disable-shared --disable-https --with-pic --disable-doc
-        COMMAND ${CMAKE_COMMAND} -E env
-          CC=${CMAKE_C_COMPILER}
-          CXX=${CMAKE_CXX_COMPILER}
-          make clean
-        COMMAND ${CMAKE_COMMAND} -E env
-          CC=${CMAKE_C_COMPILER}
-          CXX=${CMAKE_CXX_COMPILER}
-          make -j16
-        COMMENT "Build libmicrohttpd"
-        DEPENDS ${MHD_SOURCE_DIR}
-        OUTPUT ${MHD_LIBRARY}
+      WORKING_DIRECTORY ${MHD_BINARY_DIR}
+      COMMAND ${CMAKE_COMMAND} -E rm -rf ${MHD_BUILD_DIR}
+      COMMAND ${CMAKE_COMMAND} -E copy_directory ${MHD_SOURCE_DIR} ${MHD_BUILD_DIR}
+      COMMAND ${CMAKE_COMMAND} -E rm -f ${MHD_LIBRARY}
+      COMMAND ${CMAKE_COMMAND} -E chdir ${MHD_BUILD_DIR} ${CMAKE_COMMAND} -E env
+        CC=${MHD_CC}
+        CXX=${MHD_CXX}
+        AR=${MHD_AR}
+        RANLIB=${MHD_RANLIB}
+        CFLAGS=${MHD_CFLAGS}
+        ./configure ${MHD_CONFIGURE_ARGS}
+      COMMAND ${CMAKE_COMMAND} -E chdir ${MHD_BUILD_DIR} ${CMAKE_COMMAND} -E env
+        CC=${MHD_CC}
+        CXX=${MHD_CXX}
+        AR=${MHD_AR}
+        RANLIB=${MHD_RANLIB}
+        CFLAGS=${MHD_CFLAGS}
+        ACLOCAL=:
+        AUTOMAKE=:
+        AUTOCONF=:
+        AUTOHEADER=:
+        make clean
+      COMMAND ${CMAKE_COMMAND} -E chdir ${MHD_BUILD_DIR} ${CMAKE_COMMAND} -E env
+        CC=${MHD_CC}
+        CXX=${MHD_CXX}
+        AR=${MHD_AR}
+        RANLIB=${MHD_RANLIB}
+        CFLAGS=${MHD_CFLAGS}
+        ACLOCAL=:
+        AUTOMAKE=:
+        AUTOCONF=:
+        AUTOHEADER=:
+        make -j16
+      COMMAND ${CMAKE_COMMAND} -E chdir ${MHD_BUILD_DIR} ${CMAKE_COMMAND} -E env
+        CC=${MHD_CC}
+        CXX=${MHD_CXX}
+        AR=${MHD_AR}
+        RANLIB=${MHD_RANLIB}
+        CFLAGS=${MHD_CFLAGS}
+        ACLOCAL=:
+        AUTOMAKE=:
+        AUTOCONF=:
+        AUTOHEADER=:
+        make install
+      ${MHD_POST_INSTALL_COMMANDS}
+      COMMENT "Build libmicrohttpd"
+      DEPENDS ${MHD_SOURCE_DIR}
+      OUTPUT ${MHD_LIBRARY}
     )
   endif()
 endif()
--- CMake/BuildSodium.cmake
+++ CMake/BuildSodium.cmake
@@ -6,9 +6,10 @@ set(TON_THIRD_PARTY_BINARY_DIR "${CMAKE_BINARY_DIR}/third-party")
 
 set(SODIUM_SOURCE_DIR ${TON_THIRD_PARTY_SOURCE_DIR}/sodium)
 set(SODIUM_BINARY_DIR ${TON_THIRD_PARTY_BINARY_DIR}/sodium)
+set(SODIUM_BUILD_DIR ${SODIUM_BINARY_DIR}/src)
 
 if (USE_EMSCRIPTEN OR EMSCRIPTEN)
-  set(SODIUM_ROOT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/3pp_emscripten/libsodium)
+  set(SODIUM_ROOT_DIR ${CMAKE_BINARY_DIR}/3pp_emscripten/libsodium)
   if (EXISTS ${SODIUM_ROOT_DIR}/lib/libsodium.a)
     set(SODIUM_LIBRARY ${SODIUM_ROOT_DIR}/lib/libsodium.a)
     set(SODIUM_INCLUDE_DIR ${SODIUM_ROOT_DIR}/include)
@@ -24,15 +25,18 @@ if (USE_EMSCRIPTEN OR EMSCRIPTEN)
   set(SODIUM_FOUND TRUE CACHE BOOL "Sodium found" FORCE)
 elseif (MSVC)
   set(SODIUM_PROJECT_DIR ${SODIUM_SOURCE_DIR}/builds/msvc/vs2022/libsodium)
-  set(SODIUM_MSVC_OUT_DIR ${SODIUM_PROJECT_DIR}/x64/Release)
-  set(SODIUM_LIBRARY ${SODIUM_MSVC_OUT_DIR}/libsodium.lib CACHE FILEPATH "Sodium release library" FORCE)
+  set(SODIUM_LIBRARY ${SODIUM_BINARY_DIR}/lib/libsodium.lib CACHE FILEPATH "Sodium release library" FORCE)
   set(SODIUM_LIBRARY_RELEASE ${SODIUM_LIBRARY} CACHE FILEPATH "Sodium release library" FORCE)
   set(SODIUM_LIBRARY_DEBUG ${SODIUM_LIBRARY} CACHE FILEPATH "Sodium debug library" FORCE)
   set(SODIUM_INCLUDE_DIR ${SODIUM_SOURCE_DIR}/src/libsodium/include CACHE PATH "Sodium include dir" FORCE)
   set(SODIUM_FOUND TRUE CACHE BOOL "Sodium found" FORCE)
+  file(MAKE_DIRECTORY ${SODIUM_BINARY_DIR}/lib)
+  file(MAKE_DIRECTORY ${SODIUM_BINARY_DIR}/obj)
+  set(SODIUM_MSVC_OUT_DIR "${SODIUM_BINARY_DIR}/lib/")
+  set(SODIUM_MSVC_INT_DIR "${SODIUM_BINARY_DIR}/obj/")
   if (NOT EXISTS "${SODIUM_LIBRARY}")
     execute_process(
-      COMMAND msbuild libsodium.vcxproj /p:Configuration=ReleaseLIB /p:Platform=x64 -p:PlatformToolset=v143 /p:OutDir=${SODIUM_MSVC_OUT_DIR}\\ /p:IntDir=${SODIUM_MSVC_OUT_DIR}\\obj\\
+      COMMAND msbuild libsodium.vcxproj /p:Configuration=ReleaseLIB /p:Platform=x64 /p:PlatformToolset=v143 /p:OutDir=${SODIUM_MSVC_OUT_DIR} /p:IntDir=${SODIUM_MSVC_INT_DIR}
       WORKING_DIRECTORY ${SODIUM_PROJECT_DIR}
       RESULT_VARIABLE SODIUM_BUILD_RESULT
     )
@@ -42,13 +46,35 @@ elseif (MSVC)
   endif()
   add_custom_command(
     WORKING_DIRECTORY ${SODIUM_PROJECT_DIR}
-    COMMAND msbuild libsodium.vcxproj /p:Configuration=ReleaseLIB /p:Platform=x64 -p:PlatformToolset=v143 /p:OutDir=${SODIUM_MSVC_OUT_DIR}\\ /p:IntDir=${SODIUM_MSVC_OUT_DIR}\\obj\\
+    COMMAND msbuild libsodium.vcxproj /p:Configuration=ReleaseLIB /p:Platform=x64 /p:PlatformToolset=v143 /p:OutDir=${SODIUM_MSVC_OUT_DIR} /p:IntDir=${SODIUM_MSVC_INT_DIR}
     COMMENT "Build sodium (MSVC)"
     DEPENDS ${SODIUM_SOURCE_DIR}
     OUTPUT ${SODIUM_LIBRARY}
   )
-elseif (ANDROID)
-  set(SODIUM_BINARY_DIR ${TON_ANDROID_THIRD_PARTY_DIR}/libsodium/${TON_ANDROID_SODIUM_DIR})
+elseif (ANDROID OR NOT NIX)
+  if (ANDROID)
+    set(SODIUM_BINARY_DIR ${TON_ANDROID_THIRD_PARTY_DIR}/libsodium/${TON_ANDROID_SODIUM_DIR})
+    set(SODIUM_BUILD_DIR ${SODIUM_BINARY_DIR}/src)
+    set(SODIUM_CC ${TON_ANDROID_CC})
+    set(SODIUM_CXX ${TON_ANDROID_CXX})
+    set(SODIUM_AR ${TON_ANDROID_AR})
+    set(SODIUM_RANLIB ${TON_ANDROID_RANLIB})
+  elseif (MINGW)
+    set(SODIUM_CC ${CMAKE_C_COMPILER})
+    set(SODIUM_CXX ${CMAKE_CXX_COMPILER})
+    set(SODIUM_AR ar)
+    set(SODIUM_RANLIB ranlib)
+  else()
+    set(SODIUM_CC ${CMAKE_C_COMPILER})
+    set(SODIUM_CXX ${CMAKE_CXX_COMPILER})
+    set(SODIUM_AR ${CMAKE_AR})
+    if (CMAKE_RANLIB)
+      set(SODIUM_RANLIB ${CMAKE_RANLIB})
+    else()
+      set(SODIUM_RANLIB ranlib)
+    endif()
+  endif()
+
   set(SODIUM_INCLUDE_DIR ${SODIUM_BINARY_DIR}/include)
   set(SODIUM_LIBRARY ${SODIUM_BINARY_DIR}/lib/libsodium.a CACHE FILEPATH "Sodium release library" FORCE)
   set(SODIUM_LIBRARY_RELEASE ${SODIUM_LIBRARY} CACHE FILEPATH "Sodium release library" FORCE)
@@ -56,11 +82,10 @@ elseif (ANDROID)
   set(SODIUM_INCLUDE_DIR ${SODIUM_INCLUDE_DIR} CACHE PATH "Sodium include dir" FORCE)
   set(SODIUM_FOUND TRUE CACHE BOOL "Sodium found" FORCE)
 
-  file(MAKE_DIRECTORY ${SODIUM_BINARY_DIR})
-  file(MAKE_DIRECTORY "${SODIUM_BINARY_DIR}/include")
+  file(MAKE_DIRECTORY ${SODIUM_BINARY_DIR}/lib)
+  file(MAKE_DIRECTORY ${SODIUM_INCLUDE_DIR})
+  file(MAKE_DIRECTORY ${SODIUM_BUILD_DIR})
 
-  set(SODIUM_AR ${TON_ANDROID_AR})
-  set(SODIUM_RANLIB ${TON_ANDROID_RANLIB})
   if (CMAKE_C_FLAGS)
     set(SODIUM_CFLAGS "${CMAKE_C_FLAGS} -fPIC")
   else()
@@ -71,140 +96,82 @@ elseif (ANDROID)
   else()
     set(SODIUM_CXXFLAGS "-fPIC")
   endif()
-  add_custom_command(
-      WORKING_DIRECTORY ${SODIUM_SOURCE_DIR}
+
+  set(SODIUM_CONFIGURE_ARGS
+    --prefix=${SODIUM_BINARY_DIR}
+    --with-pic
+    --enable-static
+    --disable-shared
+  )
+  if (ANDROID)
+    list(APPEND SODIUM_CONFIGURE_ARGS --host=${TON_ANDROID_HOST})
+  endif()
+
+  if (MINGW)
+    find_program(MSYS2_BASH bash)
+    if (NOT MSYS2_BASH)
+      message(FATAL_ERROR "bash not found in PATH; ensure MSYS2 is in PATH.")
+    endif()
+    string(REPLACE ";" " " SODIUM_CONFIGURE_ARGS_STR "${SODIUM_CONFIGURE_ARGS}")
+    add_custom_command(
+      WORKING_DIRECTORY ${SODIUM_BINARY_DIR}
+      COMMAND ${CMAKE_COMMAND} -E rm -rf ${SODIUM_BUILD_DIR}
+      COMMAND ${CMAKE_COMMAND} -E copy_directory ${SODIUM_SOURCE_DIR} ${SODIUM_BUILD_DIR}
+      COMMAND ${MSYS2_BASH} -lc "cd '${SODIUM_BUILD_DIR}' && CC='${SODIUM_CC}' CXX='${SODIUM_CXX}' AR='${SODIUM_AR}' RANLIB='${SODIUM_RANLIB}' CFLAGS='${SODIUM_CFLAGS}' CXXFLAGS='${SODIUM_CXXFLAGS}' ./configure ${SODIUM_CONFIGURE_ARGS_STR}"
+      COMMAND ${MSYS2_BASH} -lc "cd '${SODIUM_BUILD_DIR}' && CC='${SODIUM_CC}' CXX='${SODIUM_CXX}' AR='${SODIUM_AR}' RANLIB='${SODIUM_RANLIB}' CFLAGS='${SODIUM_CFLAGS}' CXXFLAGS='${SODIUM_CXXFLAGS}' make clean"
+      COMMAND ${MSYS2_BASH} -lc "cd '${SODIUM_BUILD_DIR}' && CC='${SODIUM_CC}' CXX='${SODIUM_CXX}' AR='${SODIUM_AR}' RANLIB='${SODIUM_RANLIB}' CFLAGS='${SODIUM_CFLAGS}' CXXFLAGS='${SODIUM_CXXFLAGS}' make -j16"
+      COMMAND ${MSYS2_BASH} -lc "cd '${SODIUM_BUILD_DIR}' && CC='${SODIUM_CC}' CXX='${SODIUM_CXX}' AR='${SODIUM_AR}' RANLIB='${SODIUM_RANLIB}' CFLAGS='${SODIUM_CFLAGS}' CXXFLAGS='${SODIUM_CXXFLAGS}' make install"
+      COMMAND ${CMAKE_COMMAND} -E echo "Skip ranlib on MinGW"
+      COMMENT "Build sodium"
+      DEPENDS ${SODIUM_SOURCE_DIR}
+      OUTPUT ${SODIUM_LIBRARY}
+    )
+  else()
+    add_custom_command(
+      WORKING_DIRECTORY ${SODIUM_BINARY_DIR}
+      COMMAND ${CMAKE_COMMAND} -E rm -rf ${SODIUM_BUILD_DIR}
+      COMMAND ${CMAKE_COMMAND} -E copy_directory ${SODIUM_SOURCE_DIR} ${SODIUM_BUILD_DIR}
       COMMAND ${CMAKE_COMMAND} -E rm -f ${SODIUM_LIBRARY}
-      COMMAND ${CMAKE_COMMAND} -E env
-        CC=${TON_ANDROID_CC}
-        CXX=${TON_ANDROID_CXX}
+      COMMAND ${CMAKE_COMMAND} -E chdir ${SODIUM_BUILD_DIR} ${CMAKE_COMMAND} -E env
+        CC=${SODIUM_CC}
+        CXX=${SODIUM_CXX}
         AR=${SODIUM_AR}
         RANLIB=${SODIUM_RANLIB}
         CFLAGS=${SODIUM_CFLAGS}
         CXXFLAGS=${SODIUM_CXXFLAGS}
-        ./autogen.sh
-      COMMAND ${CMAKE_COMMAND} -E env
-        CC=${TON_ANDROID_CC}
-        CXX=${TON_ANDROID_CXX}
-        AR=${SODIUM_AR}
-        RANLIB=${SODIUM_RANLIB}
-        CFLAGS=${SODIUM_CFLAGS}
-        CXXFLAGS=${SODIUM_CXXFLAGS}
-        ./configure --host=${TON_ANDROID_HOST} --prefix=${SODIUM_BINARY_DIR} --with-pic --enable-static --disable-shared
-      COMMAND ${CMAKE_COMMAND} -E env
-        CC=${TON_ANDROID_CC}
-        CXX=${TON_ANDROID_CXX}
+        ./configure ${SODIUM_CONFIGURE_ARGS}
+      COMMAND ${CMAKE_COMMAND} -E chdir ${SODIUM_BUILD_DIR} ${CMAKE_COMMAND} -E env
+        CC=${SODIUM_CC}
+        CXX=${SODIUM_CXX}
         AR=${SODIUM_AR}
         RANLIB=${SODIUM_RANLIB}
         CFLAGS=${SODIUM_CFLAGS}
         CXXFLAGS=${SODIUM_CXXFLAGS}
         make clean
-      COMMAND ${CMAKE_COMMAND} -E env
-        CC=${TON_ANDROID_CC}
-        CXX=${TON_ANDROID_CXX}
+      COMMAND ${CMAKE_COMMAND} -E chdir ${SODIUM_BUILD_DIR} ${CMAKE_COMMAND} -E env
+        CC=${SODIUM_CC}
+        CXX=${SODIUM_CXX}
         AR=${SODIUM_AR}
         RANLIB=${SODIUM_RANLIB}
         CFLAGS=${SODIUM_CFLAGS}
         CXXFLAGS=${SODIUM_CXXFLAGS}
         make -j16
-      COMMAND ${CMAKE_COMMAND} -E env
-        CC=${TON_ANDROID_CC}
-        CXX=${TON_ANDROID_CXX}
+      COMMAND ${CMAKE_COMMAND} -E chdir ${SODIUM_BUILD_DIR} ${CMAKE_COMMAND} -E env
+        CC=${SODIUM_CC}
+        CXX=${SODIUM_CXX}
         AR=${SODIUM_AR}
         RANLIB=${SODIUM_RANLIB}
         CFLAGS=${SODIUM_CFLAGS}
         CXXFLAGS=${SODIUM_CXXFLAGS}
         make install
       COMMAND ${SODIUM_RANLIB} ${SODIUM_LIBRARY}
-      COMMENT "Build sodium (Android)"
+      COMMENT "Build sodium"
       DEPENDS ${SODIUM_SOURCE_DIR}
       OUTPUT ${SODIUM_LIBRARY}
-  )
-elseif (NOT NIX)
-  set(SODIUM_INCLUDE_DIR ${SODIUM_BINARY_DIR}/include)
-  set(SODIUM_LIBRARY ${SODIUM_BINARY_DIR}/lib/libsodium.a CACHE FILEPATH "Sodium release library" FORCE)
-  set(SODIUM_LIBRARY_RELEASE ${SODIUM_LIBRARY} CACHE FILEPATH "Sodium release library" FORCE)
-  set(SODIUM_LIBRARY_DEBUG ${SODIUM_LIBRARY} CACHE FILEPATH "Sodium debug library" FORCE)
-  set(SODIUM_INCLUDE_DIR ${SODIUM_INCLUDE_DIR} CACHE PATH "Sodium include dir" FORCE)
-  set(SODIUM_FOUND TRUE CACHE BOOL "Sodium found" FORCE)
-
-  file(MAKE_DIRECTORY ${SODIUM_BINARY_DIR})
-  file(MAKE_DIRECTORY "${SODIUM_BINARY_DIR}/include")
-
-  if (MINGW)
-    set(SODIUM_AR ar)
-    set(SODIUM_RANLIB ranlib)
-    find_program(MSYS2_BASH bash)
-    if (NOT MSYS2_BASH)
-      message(FATAL_ERROR "bash not found in PATH; ensure MSYS2 is in PATH.")
-    endif()
-  elseif (CMAKE_RANLIB)
-    set(SODIUM_AR ${CMAKE_AR})
-    set(SODIUM_RANLIB ${CMAKE_RANLIB})
-  else()
-    set(SODIUM_AR ${CMAKE_AR})
-    set(SODIUM_RANLIB ranlib)
-  endif()
-  if (MINGW)
-    set(SODIUM_POST_RANLIB ${CMAKE_COMMAND} -E echo "Skip ranlib on MinGW")
-  else()
-    set(SODIUM_POST_RANLIB ${SODIUM_RANLIB} ${SODIUM_LIBRARY})
-  endif()
-  if (MINGW)
-    add_custom_command(
-        WORKING_DIRECTORY ${SODIUM_SOURCE_DIR}
-        COMMAND ${CMAKE_COMMAND} -E rm -f ${SODIUM_LIBRARY}
-        COMMAND ${MSYS2_BASH} -lc "CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} AR=${SODIUM_AR} RANLIB=${SODIUM_RANLIB} ./autogen.sh"
-        COMMAND ${MSYS2_BASH} -lc "CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} AR=${SODIUM_AR} RANLIB=${SODIUM_RANLIB} ./configure --prefix=${SODIUM_BINARY_DIR} --with-pic --enable-static --disable-shared"
-        COMMAND ${MSYS2_BASH} -lc "CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} AR=${SODIUM_AR} RANLIB=${SODIUM_RANLIB} make clean"
-        COMMAND ${MSYS2_BASH} -lc "CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} AR=${SODIUM_AR} RANLIB=${SODIUM_RANLIB} make -j16"
-        COMMAND ${MSYS2_BASH} -lc "CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} AR=${SODIUM_AR} RANLIB=${SODIUM_RANLIB} make install"
-        COMMAND ${SODIUM_POST_RANLIB}
-        COMMENT "Build sodium"
-        DEPENDS ${SODIUM_SOURCE_DIR}
-        OUTPUT ${SODIUM_LIBRARY}
-    )
-  else()
-    add_custom_command(
-        WORKING_DIRECTORY ${SODIUM_SOURCE_DIR}
-        COMMAND ${CMAKE_COMMAND} -E rm -f ${SODIUM_LIBRARY}
-        COMMAND ${CMAKE_COMMAND} -E env
-          CC=${CMAKE_C_COMPILER}
-          CXX=${CMAKE_CXX_COMPILER}
-          AR=${SODIUM_AR}
-          RANLIB=${SODIUM_RANLIB}
-          ./autogen.sh
-        COMMAND ${CMAKE_COMMAND} -E env
-          CC=${CMAKE_C_COMPILER}
-          CXX=${CMAKE_CXX_COMPILER}
-          AR=${SODIUM_AR}
-          RANLIB=${SODIUM_RANLIB}
-          ./configure --prefix=${SODIUM_BINARY_DIR} --with-pic --enable-static --disable-shared
-        COMMAND ${CMAKE_COMMAND} -E env
-          CC=${CMAKE_C_COMPILER}
-          CXX=${CMAKE_CXX_COMPILER}
-          AR=${SODIUM_AR}
-          RANLIB=${SODIUM_RANLIB}
-          make clean
-        COMMAND ${CMAKE_COMMAND} -E env
-          CC=${CMAKE_C_COMPILER}
-          CXX=${CMAKE_CXX_COMPILER}
-          AR=${SODIUM_AR}
-          RANLIB=${SODIUM_RANLIB}
-          make -j16
-        COMMAND ${CMAKE_COMMAND} -E env
-          CC=${CMAKE_C_COMPILER}
-          CXX=${CMAKE_CXX_COMPILER}
-          AR=${SODIUM_AR}
-          RANLIB=${SODIUM_RANLIB}
-          make install
-        COMMAND ${SODIUM_POST_RANLIB}
-        COMMENT "Build sodium"
-        DEPENDS ${SODIUM_SOURCE_DIR}
-        OUTPUT ${SODIUM_LIBRARY}
     )
   endif()
 else()
-  message(STATUS "Use Secp256k1: ${SODIUM_LIBRARY}")
+  message(STATUS "Use sodium: ${SODIUM_LIBRARY}")
 endif()
 
 add_custom_target(sodium DEPENDS ${SODIUM_LIBRARY})
