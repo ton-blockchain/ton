3a9c87645668155c959ed99232c799bec5707a04
a1oleg <3091617+a1oleg@users.noreply.github.com>
2026-03-15 14:59:08 +0400

    Revert "Refactor CMake and build scripts for simulation setup; add error logs for missing dependencies"
    This reverts commit 639cdde510ddd986b412d8930236ebfa654695ab.

--- ninja_harness.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH=/ucrt64/bin:/usr/bin:/bin
-cd /c/GitHub/tonGraph/build
-ninja third-party/zlib/lib/libz.a -j4
-echo "ZLIB_EXIT:$?"
-ninja ConsensusHarness -j4
-echo "NINJA_EXIT:$?"
