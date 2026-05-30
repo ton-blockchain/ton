dcb05be22459dbfe2867cb2d6fcb611b478c54a0
neodix42 <namlem@gmail.com>
2026-05-30 18:47:48 +0400

    follow up Docker image improvements (#2405)
    * support custom quic port in Docker image
    
    * update custom quic and liteserver port in Docker image on every start
    
    * add more categories for quic addresses
    
    * rework configuration of control interface in Docker
    
    * harden docker/init.sh to avoid config.json corruption
    
    * improve config.json handling in Docker
    
    * no need to derive and copy existing keys

--- docker/init.sh
+++ docker/init.sh
@@ -52,6 +52,8 @@ if [[ "$PUBLIC_IP" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
         QUIC_IP_INT=$((QUIC_IP_INT - 4294967296))
     fi
 
+    QUIC_CONFIG_TMP=$(mktemp /var/ton-work/db/config.json.quic.XXXXXX)
+    test $? -eq 0 || { echo "Cannot create temporary QUIC config file"; exit 2; }
     jq --argjson quic_ip "$QUIC_IP_INT" --argjson quic_port "$QUIC_PORT" '
       .addrs = (
         (.addrs // []) as $addrs
@@ -65,9 +67,10 @@ if [[ "$PUBLIC_IP" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
           else $addrs + [{"@type":"engine.quicAddr","ip":$quic_ip,"port":$quic_port,"categories":[0, 1, 2, 3],"priority_categories":[]}]
           end
       )
-    ' /var/ton-work/db/config.json > config.json.quic
-    test $? -eq 0 || { echo "Cannot apply QUIC address config"; exit 2; }
-    mv config.json.quic /var/ton-work/db/config.json
+    ' /var/ton-work/db/config.json > "$QUIC_CONFIG_TMP"
+    test $? -eq 0 || { rm -f "$QUIC_CONFIG_TMP"; echo "Cannot apply QUIC address config"; exit 2; }
+    mv "$QUIC_CONFIG_TMP" /var/ton-work/db/config.json
+    test $? -eq 0 || { rm -f "$QUIC_CONFIG_TMP"; echo "Cannot replace config after QUIC update"; exit 2; }
     echo -e "\e[1;32m[+]\e[0m QUIC address configured: $PUBLIC_IP:$QUIC_PORT"
 else
     echo -e "\e[1;31m[!]\e[0m PUBLIC_IP is not IPv4, skipping QUIC address configuration"
@@ -122,25 +125,71 @@ fi
 # Generating server certificate
 if [ -f "./server" ]; then
     echo -e "\e[1;33m[=]\e[0m Found existing server certificate, skipping"
+    SERVER_ID2=$(generate-random-id -m id -k ./server | sed -n '3p' | jq -r '.id')
+    test $? -eq 0 || { echo "Cannot derive server public id from existing certificate"; exit 2; }
+    if [ -z "$SERVER_ID2" ] || [ "$SERVER_ID2" = "null" ]; then
+        echo "Cannot derive server public id from existing certificate"
+        exit 2
+    fi
 else
     echo -e "\e[1;32m[+]\e[0m Generating and installing server certificate for remote control"
     read -r SERVER_ID1 SERVER_ID2 <<< $(generate-random-id -m keys -n server)
     echo "Server IDs: $SERVER_ID1 $SERVER_ID2"
     cp server /var/ton-work/db/keyring/$SERVER_ID1
+    test $? -eq 0 || { echo "Cannot install server private key into keyring"; exit 2; }
 fi
 
 # Generating client certificate
 if [ -f "./client" ]; then
     echo -e "\e[1;33m[=]\e[0m Found existing client certificate, skipping"
+    CLIENT_ID2=$(generate-random-id -m id -k ./client | sed -n '3p' | jq -r '.id')
+    test $? -eq 0 || { echo "Cannot derive client public id from existing certificate"; exit 2; }
+    if [ -z "$CLIENT_ID2" ] || [ "$CLIENT_ID2" = "null" ]; then
+        echo "Cannot derive client public id from existing certificate"
+        exit 2
+    fi
 else
     read -r CLIENT_ID1 CLIENT_ID2 <<< $(generate-random-id -m keys -n client)
     echo -e "\e[1;32m[+]\e[0m Generated client private certificate $CLIENT_ID1 $CLIENT_ID2"
     echo -e "\e[1;32m[+]\e[0m Generated client public certificate"
-    # Adding client permissions
-    sed -e "s/CONSOLE-PORT/\"$(printf "%q" $CONSOLE_PORT)\"/g" -e "s~SERVER-ID~\"$(printf "%q" $SERVER_ID2)\"~g" -e "s~CLIENT-ID~\"$(printf "%q" $CLIENT_ID2)\"~g" /var/ton-work/scripts/control.template > control.new
-    sed -e "s~\"control\"\ \:\ \[~$(printf "%q" $(cat control.new))~g" /var/ton-work/db/config.json > config.json.new
-    mv config.json.new /var/ton-work/db/config.json
 fi
+
+# Configure control interface and client permissions
+CONTROL_CONFIG_TMP=$(mktemp /var/ton-work/db/config.json.control.XXXXXX)
+test $? -eq 0 || { echo "Cannot create temporary control config file"; exit 2; }
+jq --arg server_id "$SERVER_ID2" --arg client_id "$CLIENT_ID2" --argjson console_port "$CONSOLE_PORT" '
+  .control = (
+    (.control // []) as $control
+    | ($control | map(select(.port != $console_port and .id != $server_id))) as $other_controls
+    | ($control | map(select(.port == $console_port or .id == $server_id))[0] // {
+        "@type":"engine.controlInterface",
+        "id":$server_id,
+        "port":$console_port,
+        "allowed":[]
+      }) as $server_control
+    | ($server_control + {
+        "@type":"engine.controlInterface",
+        "id":$server_id,
+        "port":$console_port,
+        "allowed": (
+          ($server_control.allowed // []) as $allowed
+          | if ($allowed | any(.id == $client_id))
+            then ($allowed | map(
+              if .id == $client_id
+              then . + {"@type":"engine.controlProcess","permissions":15}
+              else .
+              end
+            ))
+            else $allowed + [{"@type":"engine.controlProcess","id":$client_id,"permissions":15}]
+            end
+        )
+      }) as $updated_server_control
+    | $other_controls + [$updated_server_control]
+  )
+' /var/ton-work/db/config.json > "$CONTROL_CONFIG_TMP"
+test $? -eq 0 || { rm -f "$CONTROL_CONFIG_TMP"; echo "Cannot apply control interface config"; exit 2; }
+mv "$CONTROL_CONFIG_TMP" /var/ton-work/db/config.json
+test $? -eq 0 || { rm -f "$CONTROL_CONFIG_TMP"; echo "Cannot replace config after control update"; exit 2; }
 
 # Liteserver
 if [ -z "$LITESERVER" ]; then
@@ -166,24 +215,30 @@ else
         read -r LITESERVER_ID1 LITESERVER_ID2 <<< $(generate-random-id -m keys -n liteserver)
         echo "Liteserver IDs: $LITESERVER_ID1 $LITESERVER_ID2"
         cp liteserver /var/ton-work/db/keyring/$LITESERVER_ID1
+        test $? -eq 0 || { echo "Cannot install liteserver private key into keyring"; exit 2; }
     fi
 
+    LITESERVER_CONFIG_TMP=$(mktemp /var/ton-work/db/config.json.liteservers.XXXXXX)
+    test $? -eq 0 || { echo "Cannot create temporary liteserver config file"; exit 2; }
     jq --arg liteserver_id "$LITESERVER_ID2" --argjson lite_port "$LITE_PORT" '
       .liteservers = (
         (.liteservers // []) as $liteservers
-        | if ($liteservers | any(.id == $liteserver_id))
-          then ($liteservers | map(
-            if .id == $liteserver_id
-            then . + {"port": $lite_port}
-            else .
-            end
-          ))
-          else $liteservers + [{"@type":"engine.liteServer","id":$liteserver_id,"port":$lite_port}]
-          end
+        | ($liteservers | map(select(.port != $lite_port and .id != $liteserver_id))) as $other_liteservers
+        | ($liteservers | map(select(.port == $lite_port or .id == $liteserver_id))[0] // {
+            "@type":"engine.liteServer",
+            "id":$liteserver_id,
+            "port":$lite_port
+          }) as $managed_liteserver
+        | $other_liteservers + [($managed_liteserver + {
+            "@type":"engine.liteServer",
+            "id":$liteserver_id,
+            "port":$lite_port
+          })]
       )
-    ' /var/ton-work/db/config.json > config.json.liteservers
-    test $? -eq 0 || { echo "Cannot apply liteserver config"; exit 2; }
-    mv config.json.liteservers /var/ton-work/db/config.json
+    ' /var/ton-work/db/config.json > "$LITESERVER_CONFIG_TMP"
+    test $? -eq 0 || { rm -f "$LITESERVER_CONFIG_TMP"; echo "Cannot apply liteserver config"; exit 2; }
+    mv "$LITESERVER_CONFIG_TMP" /var/ton-work/db/config.json
+    test $? -eq 0 || { rm -f "$LITESERVER_CONFIG_TMP"; echo "Cannot replace config after liteserver update"; exit 2; }
 fi
 
 echo -e "\e[1;32m[+]\e[0m Starting validator-engine:"
