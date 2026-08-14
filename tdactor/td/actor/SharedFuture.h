8180ecf3bfe3249fcd8fa25114bebd56b3ee029c
SpyCheese <mikle98@yandex.ru>
2026-08-14 19:40:58 +0300

    Fix race in await_with_timeout

--- tdactor/td/actor/SharedFuture.h
+++ tdactor/td/actor/SharedFuture.h
@@ -60,19 +60,26 @@ constexpr int AWAIT_TIMEOUT_CODE = 6520;
 template <typename T>
 Task<T> await_with_timeout(StartedTask<T> task, Timestamp timeout) {
   auto [task_result, promise] = StartedTask<T>::make_bridge();
-  auto promise_ptr = std::make_shared<Promise<T>>(std::move(promise));
+  auto promise_ptr = std::make_shared<std::pair<Promise<T>, std::atomic_flag>>(std::move(promise), false);
   if (timeout) {
-    auto worker_timeout = [](Timestamp timeout, std::shared_ptr<Promise<T>> promise_ptr) -> Task<> {
-      co_await td::actor::detach_from_actor();
+    auto worker_timeout = [](Timestamp timeout,
+                             std::shared_ptr<std::pair<Promise<T>, std::atomic_flag>> promise_ptr) -> Task<> {
+      co_await detach_from_actor();
       co_await coro_sleep(timeout);
-      promise_ptr->set_error(Status::Error(AWAIT_TIMEOUT_CODE, "await timeout"));
+      if (!promise_ptr->second.test_and_set()) {
+        promise_ptr->first.set_error(Status::Error(AWAIT_TIMEOUT_CODE, "await timeout"));
+      }
       co_return {};
     };
     worker_timeout(timeout, promise_ptr).start().detach_silent();
   }
-  auto worker_wait = [](StartedTask<T> task, std::shared_ptr<Promise<T>> promise_ptr) -> Task<> {
-    co_await td::actor::detach_from_actor();
-    promise_ptr->set_result(co_await std::move(task).wrap());
+  auto worker_wait = [](StartedTask<T> task,
+                        std::shared_ptr<std::pair<Promise<T>, std::atomic_flag>> promise_ptr) -> Task<> {
+    co_await detach_from_actor();
+    auto result = co_await std::move(task).wrap();
+    if (!promise_ptr->second.test_and_set()) {
+      promise_ptr->first.set_result(std::move(result));
+    }
     co_return {};
   };
   worker_wait(std::move(task), std::move(promise_ptr)).start().detach_silent();
