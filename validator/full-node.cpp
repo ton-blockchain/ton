f6b593e87d06360fe115a410f8ce97c7fc523b78
SpyCheese <mikle98@yandex.ru>
2026-09-22 14:02:12 +0300

    Prepare transition from rldp2 to quic in custom overlays

--- validator/full-node.cpp
+++ validator/full-node.cpp
@@ -1110,9 +1110,8 @@ void FullNodeImpl::update_custom_overlay(CustomOverlayInfo &overlay) {
         overlay.actors_[local_id] = std::move(it->second);
         old_actors.erase(it);
       } else {
-        auto adnl_sender = (params.use_quic_ ? td::actor::ActorId<adnl::AdnlSenderEx>{quic_} : rldp2_);
         overlay.actors_[local_id] = td::actor::create_actor<FullNodeCustomOverlay>(
-            "CustomOverlay", local_id, params, zero_state_file_hash_, opts_, keyring_, adnl_, adnl_sender, overlays_,
+            "CustomOverlay", local_id, params, zero_state_file_hash_, opts_, keyring_, adnl_, quic_, rldp2_, overlays_,
             validator_manager_, actor_id(this));
       }
     }
